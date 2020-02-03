de1f71d039ee727acb806dc97e7caf0721b0fb9f
rui-kanoh <rui.kanoh@gmail.com>
2020-02-03 17:04:46 +0900

    Runtime Error fix

--- csharp/AtCoderCSharp/AtCoderCSharp/Program.cs
+++ csharp/AtCoderCSharp/AtCoderCSharp/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AtCoderCSharp
 {
@@ -11,34 +9,23 @@ namespace AtCoderCSharp
 	{
 		static void Main(string[] args)
 		{
-			using (var writer = new StreamWriter(args[1])) {
-				List<int> intDataList = new List<int>();
-				string strData = "";
-				using (var reader = new StreamReader(args[0])) {
-					// Redirect standard output from the console to the output file.
-					Console.SetOut(writer);
-					// Redirect standard input from the console to the input file.
-					Console.SetIn(reader);
-
-					for (int i = 0; i < 2; ++i) {
-						string line = Console.ReadLine();
-						string[] strs = line.Split(' ');
-						foreach (var str in strs) {
-							int data = int.Parse(str);
-							intDataList.Add(data);
-							System.Diagnostics.Trace.WriteLine($"{data}");
-						}
-					}
-
-					{
-						string line = Console.ReadLine();
-						System.Diagnostics.Trace.WriteLine(line);
-						strData = line;
-					}
+			List<int> intDataList = new List<int>();
+			string strData;
+			for (int i = 0; i < 2; ++i) {
+				string line = Console.ReadLine();
+				string[] strs = line.Split(' ');
+				foreach (var str in strs) {
+					int data = int.Parse(str);
+					intDataList.Add(data);
 				}
-
-				writer.WriteLine($"{intDataList.Sum()} " + strData);
 			}
+
+			{
+				string line = Console.ReadLine();
+				strData = line;
+			}
+
+			Console.WriteLine($"{intDataList.Sum()} " + strData);
 		}
 	}
 }
