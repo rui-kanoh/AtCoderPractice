5441d1658c84bcc576ccbc02cc6f3b146278109a
elionix-kanoh <272-kanoh@elionix.co.jp>
2020-02-12 10:03:05 +0900

    Contest8

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -8,39 +8,152 @@ namespace AtCoder
 	{
 		static void Main(string[] args)
 		{
+			var Instance = new SS();
+			Instance.Exec();
+		}
+	}
+
+	public class SS
+	{
+		public void Exec()
+		{
+			string s = Console.ReadLine();
+			string gu = s;
+			bool isGu = false;
+			int max = 0;
+
+			Console.WriteLine($"{s.Length}");
+
+			for (var i = 0; i < s.Length / 2; ++i) {
+				int pivot = new Random().Next(gu.Length);
+				gu = s.Remove(pivot, 1);
+				pivot = new Random().Next(gu.Length);
+				gu = gu.Remove(pivot, 1);
+				string sub = gu.Substring(0, gu.Length / 2);
+				if (gu.Contains(sub)) {
+					isGu = true;
+				} else {
+					pivot = new Random().Next(gu.Length);
+					gu = s.Remove(pivot, 1);
+					pivot = new Random().Next(gu.Length);
+					gu = gu.Remove(pivot, 1);
+					sub = gu.Substring(0, gu.Length / 2);
+
+					if (gu.Contains(sub)) {
+						isGu = true;
+					}
+				}
+
+				if (isGu && max < gu.Length) {
+					max = gu.Length;
+				}
+
+				Console.WriteLine($"{i}, {max}, {gu}, {sub}");
+			}
+
+			Console.WriteLine($"{max}");
+
+			Console.Out.Flush();
+
+			Console.ReadLine();
+		}
+	}
+}
+
+namespace Temp {
+
+	public class Bridge
+	{
+		public void Exec()
+		{
+			var nm = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+			long n = nm[0];
+			long m = nm[1];
+			List<long> alist = new List<long>();
+			List<long> blist = new List<long>();
+			for (var j = 0; j < m; ++j) {
+				var ab = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+				alist.Add(ab[0]);
+				blist.Add(ab[1]);
+			}
+
+			Console.Out.Flush();
+
+			Console.ReadLine();
+		}
+	}
+
+	public class SplittingPile
+	{
+		public void Exec()
+		{
 			long n = long.Parse(Console.ReadLine());
 			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
 
-			long sum = 0;
+			long sum = array.Sum();
+			//Console.WriteLine($"{sum}");
+			//int pivot = new Random().Next(n);
 
-			for (var j = 0; j <= n; ++j) {
-				if (j == 0) {
-					sum += Math.Abs(array[j]);
-				} else if (j == n) {
-					sum += Math.Abs(array[j - 1]);
-				} else {
-					sum += Math.Abs(array[j] - array[j - 1]);
-				}
+			long min = (long)int.MaxValue;
+			long sunuke = array[0];
+			long arai = sum - sunuke;
+			long diff = Math.Abs(arai - sunuke);
+			if (min > diff) {
+				min = diff;
 			}
 
-			for (var i = 1; i <= n; ++i) {
-				var total = sum;
-				if (i == 1) {
-					total += Math.Abs(0 - array[1])
-						- Math.Abs(array[1] - array[0])
-						- Math.Abs(0 - array[0]);
-				} else if (i == n) {
-					total += Math.Abs(0 - array[n - 2])
-						- Math.Abs(array[n - 1] - array[n - 2])
-						- Math.Abs(0 - array[n - 1]);
-				} else {
-					total += Math.Abs(array[i] - array[i - 2])
-						- Math.Abs(array[i - 1] - array[i - 2])
-						- Math.Abs(array[i] - array[i - 1]);
+			//Console.WriteLine($"{sunuke}, {arai}");
+
+			for (var i = 1; i < n - 1; ++i) {
+				sunuke += array[i];
+				arai = sum - sunuke;
+				diff = Math.Abs(arai - sunuke);
+				if (min > diff) {
+					min = diff;
 				}
 
-				Console.WriteLine(total.ToString());
+				//Console.WriteLine($"{i}, {sunuke}, {arai}");
 			}
+
+			Console.WriteLine($"{min}");
+
+			Console.Out.Flush();
+
+			Console.ReadLine();
+		}
+	}
+
+	public class Grouping
+	{
+		public void Exec()
+		{
+			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			int x = array[0];
+			int y = array[1];
+			List<int> g1 = new List<int>() { 1, 3, 5, 7, 8, 10, 12 };
+			List<int> g2 = new List<int>() { 4, 6, 9, 11 };
+			List<int> g3 = new List<int>() { 2 };
+
+			bool contains = false;
+			if ((g1.Contains(x) && g1.Contains(y))
+				|| (g2.Contains(x) && g2.Contains(y))) {
+				contains = true;
+			}
+
+			Console.WriteLine(contains ? "Yes" : "No");
+
+			Console.Out.Flush();
+
+			Console.ReadLine();
+		}
+	}
+
+	public class QuestionA
+	{
+		public void Exec()
+		{
+			long n = long.Parse(Console.ReadLine());
+			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
 
 			Console.Out.Flush();
 
