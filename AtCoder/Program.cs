29417ede142f4730f161f08f143f834cb3584eb9
elionix-kanoh <272-kanoh@elionix.co.jp>
2020-03-18 09:07:53 +0900

    2

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -17,12 +17,27 @@ namespace AtCoder
 	{
 		public void Exec()
 		{
-			string s = Console.ReadLine();
+			//string s = Console.ReadLine();
 			int n = int.Parse(Console.ReadLine());
-			string[] strs = Console.ReadLine().Split(' ');
-			var array = Console.ReadLine().Split(' ').Select(iii => int.Parse(iii)).ToArray();
+			//var array = Console.ReadLine().Split(' ').Select(iii => int.Parse(iii)).ToArray();
+			string name = "atcoder";
+			Dictionary<string, int> pop = new Dictionary<string, int>();
+			int total = 0;
+			for (var i = 0; i < n; ++i) {
+				string[] strs = Console.ReadLine().Split(' ');
+				int num = int.Parse(strs[1]);
+				pop.Add(strs[0], num);
+				total += num;
+			}
 
-			//Console.WriteLine($"{count}");
+			foreach (var item in pop) {
+				if (item.Value > total / 2) {
+					name = item.Key;
+					break;
+				}
+			}
+
+			Console.WriteLine($"{name}");
 			Console.ReadKey();
 		}
 	}
