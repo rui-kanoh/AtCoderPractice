1b8fb602356a6854d0da3fb400cd2b41ffb262e8
elionix-kanoh <272-kanoh@elionix.co.jp>
2020-03-25 08:54:32 +0900

    cleanup

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -13,70 +13,10 @@ namespace AtCoder
 		}
 	}
 
-	public class Node<T>
-	{
-		public bool IsVisited { get; set; } = false;
-		public int Length { get; set; } = 0;
-		public int Id { get; set; }
-		public int ParentId { get; set; }
-		public T Value { get; set; }
-		public List<Node<T>> Children { get; } = new List<Node<T>>();
-
-		public void AddChild(Node<T> child)
-		{
-			child.ParentId = Id;
-			Children.Add(child);
-		}
-	}
-
 	public static class Question
 	{
-		public static int id = 2;
-		public static char[] chars = { 'a', 'b', 'c' };
-
-		public static void MakeTree(int max, Node<char> tree)
-		{
-			if (tree)
-
-			foreach (var c in chars) {
-				var node = new Node<char>() {
-					Id = id,
-					Value = c,
-				};
-				tree.AddChild(node);
-				++id;
-			}
-		}
-
 		public static void Exec()
 		{
-			int n = int.Parse(Console.ReadLine());
-
-			var tree = new Node<char>() {
-				Id = 1,
-				ParentId = -1,
-				Value = ' ',
-			};
-
-			
-
-			for (var i = 0; i < n; ++i) {
-				
-			}
-
-			var pass = new List<char>();
-			pass.Add(' ');
-
-			while (pass.Count != 0) {
-				foreach (var c in chars) {
-					pass.Add(c);
-					foreach (var c1 in chars) {
-						pass.Add(c1);
-					}
-				}
-			}
-
-
 			Console.ReadKey();
 		}
 	}
