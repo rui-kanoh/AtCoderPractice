b590713e88fe2954856b8614660f537ec5319351
elionix-kanoh <272-kanoh@elionix.co.jp>
2020-03-25 09:54:58 +0900

    4

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -17,19 +17,56 @@ namespace AtCoder
 	{
 		public static void Exec()
 		{
-			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
-			int m = array[0];
-			int n = array[1];
-			int N = array[2];
-
-			int total = N;
-			int answer = N;
-
-			while (total >= m) {
-				answer += total / m * n;
-				total = total / m * n + total % m;
-				//Console.WriteLine($"{total} {answer}");
+			int n = int.Parse(Console.ReadLine());
+			List<int> tlist = new List<int>();
+			for (var i = 0; i < n; ++i) {
+				tlist.Add(int.Parse(Console.ReadLine()));
 			}
+
+			int min = int.MaxValue;
+			int total = 0;
+
+			for (var i = 0; i < n; ++i) {
+				total += tlist[i];
+			}
+
+			for (var i = 0; i < n; ++i) {
+				int yaki1 = 0;
+				int yaki2 = 0;
+
+				yaki1 = tlist[i];
+				yaki2 = total - yaki1;
+
+				int yaki = Math.Max(yaki1, yaki2);
+				//Console.WriteLine($"{yaki1} {yaki2}");
+				min = Math.Min(yaki, min);
+			}
+
+			if (n > 2) {
+				for (var i = 1; i < n; ++i) {
+					int yaki1 = 0;
+					int yaki2 = 0;
+
+					yaki1 = tlist[0] + tlist[i];
+					yaki2 = total - yaki1;
+
+					int yaki = Math.Max(yaki1, yaki2);
+					//Console.WriteLine($"{yaki1} {yaki2}");
+					min = Math.Min(yaki, min);
+				}
+			}
+
+			if (n > 3) {
+				int yaki2 = tlist[n - 1];
+				int yaki1 = total - yaki2;
+
+				int yaki = Math.Max(yaki1, yaki2);
+				//Console.WriteLine($"{yaki1} {yaki2}");
+				min = Math.Min(yaki, min);
+			}
+
+
+			int answer = min;
 
 			Console.WriteLine($"{answer}");
 			Console.ReadKey();
