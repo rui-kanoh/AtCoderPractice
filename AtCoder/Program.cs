a3d167c6bd1af9ac399a96b6c5b0eb4fabaf2bf1
elionix-kanoh <272-kanoh@elionix.co.jp>
2020-04-15 13:44:59 +0900

    cleanup

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -13,51 +13,13 @@ namespace AtCoder
 		}
 	}
 
-	public class Plane
-	{
-		public long Time;
-		public long X;
-		public long Y;
-	}
-
 	public static class Question
 	{
 		public static void Exec()
 		{
 			long n = int.Parse(Console.ReadLine());
-			List<Plane> list = new List<Plane>();
-			var p = new Plane();
-			p.Time = 0;
-			p.X = 0;
-			p.Y = 0;
-			list.Add(p);
 
-			for (var j = 0; j < n; ++j) {
-				var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
-				var plane = new Plane();
-				plane.Time = array[0];
-				plane.X = array[1];
-				plane.Y = array[2];
-				list.Add(plane);
-			}
-
-			string answer = "Yes";
-			for (var j = 1; j <= n; ++j) {
-				long time = list[j].Time - list[j - 1].Time;
-				long x = Math.Abs(list[j].X - list[j - 1].X);
-				long y = Math.Abs(list[j].Y - list[j - 1].Y);
-				if (x + y > time) {
-					answer = "No";
-					break;
-				}
-
-				if ((x + y) % 2 != time % 2) {
-					answer = "No";
-					break;
-				}
-			}
-
-			Console.WriteLine($"{answer}");
+			//Console.WriteLine($"{answer}");
 
 			Console.ReadKey();
 		}
