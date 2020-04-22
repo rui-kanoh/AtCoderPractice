39a26ff5c29b42d9c1fb12d29dd595b6c3349fe9
rui.kanoh <rui.kanoh@gmail.com>
2020-04-22 11:05:06 +0900

    use saito scanner

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -23,8 +24,9 @@ namespace AtCoder
 
 		public static void Exec()
 		{
-			var n = long.Parse(Console.ReadLine());
-			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+			var scaner = new Scanner2();
+			var n = scaner.Long();
+			var array = scaner.ArrayLong((int)n);
 			var list = array.ToList();
 
 			var oddList = list.Where(x => IsOdd(x)).ToList();
@@ -53,6 +55,88 @@ namespace AtCoder
 			}
 
 			Console.ReadKey();
+		}
+
+		public class Scanner2
+		{
+			private readonly char[] delimiter_ = new char[] { ' ' };
+			private readonly string filePath_;
+			private readonly Func<string> reader_;
+			private string[] buf_;
+			private int index_;
+
+			public Scanner2(string file = "")
+			{
+				if (string.IsNullOrWhiteSpace(file))
+				{
+					reader_ = Console.ReadLine;
+				}
+				else
+				{
+					filePath_ = file;
+					var fs = new StreamReader(file);
+					reader_ = fs.ReadLine;
+				}
+				buf_ = new string[0];
+				index_ = 0;
+			}
+
+			public string Next()
+			{
+				if (index_ < buf_.Length)
+				{
+					return buf_[index_++];
+				}
+
+				string st = reader_();
+				while (st == "")
+				{
+					st = reader_();
+				}
+
+				buf_ = st.Split(delimiter_, StringSplitOptions.RemoveEmptyEntries);
+				if (buf_.Length == 0)
+				{
+					return Next();
+				}
+
+				index_ = 0;
+				return buf_[index_++];
+			}
+
+			public int Int() => int.Parse(Next());
+			public long Long() => long.Parse(Next());
+			public double Double() => double.Parse(Next());
+
+			public int[] ArrayInt(int N, int add = 0)
+			{
+				int[] Array = new int[N];
+				for (int i = 0; i < N; i++)
+				{
+					Array[i] = Int() + add;
+				}
+				return Array;
+			}
+
+			public long[] ArrayLong(int N, long add = 0)
+			{
+				long[] Array = new long[N];
+				for (int i = 0; i < N; i++)
+				{
+					Array[i] = Long() + add;
+				}
+				return Array;
+			}
+
+			public double[] ArrayDouble(int N, double add = 0)
+			{
+				double[] Array = new double[N];
+				for (int i = 0; i < N; i++)
+				{
+					Array[i] = Double() + add;
+				}
+				return Array;
+			}
 		}
 	}
 }
