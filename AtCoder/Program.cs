50fcfcbc57aa0dfef462d597df31fa344b0683dd
rui.kanoh <rui.kanoh@gmail.com>
2020-06-17 13:59:35 +0900

    Contest24

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -20,16 +20,33 @@ namespace AtCoder
 	{
 		public static void Exec()
 		{
-			string s = Console.ReadLine();
-			int count = 0;
-			for (int i = 0; i < 3; ++i) {
-				if (s[i] == '1') {
-					++count;
+			long n = int.Parse(Console.ReadLine());
+			var a = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+			long area = 0;
+
+			var dict = new Dictionary<long, int>();
+			foreach (var item in a) {
+				if (dict.ContainsKey(item) == false) {
+					dict.Add(item, 1);
+				} else {
+					++dict[item];
 				}
 			}
 
-			Console.WriteLine($"{count}");
+			var list = new List<long>();
+			foreach (var item in dict) {
+				if (item.Value >= 2) {
+					list.Add(item.Key);
+				}
+			}
 
+			if (list.Count >= 2) {
+				for (var i = 0; i < list.Count - 1; i = i + 2) {
+					area += list[i] * list[i + 1];
+				}
+			}
+
+			Console.WriteLine($"{area}");
 
 			Console.ReadKey();
 		}
@@ -65,12 +82,69 @@ namespace AtCoder {
 
 		public static void A()
 		{
+			var nl = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			var n = nl[0];
+			var l = nl[1];
+			string s = Console.ReadLine();
+			char[] chars = s.ToCharArray();
+			int count = 1;
+			int crash = 0;
+			foreach (var item in chars) {
+				if (item == '+') {
+					++count;
+					if (count > l) {
+						++crash;
+						count = 1;
+					}
+				} else {
+					--count;
+				}
+			}
 
+			Console.WriteLine($"{crash}");
+
+			Console.ReadKey();
 		}
 
 		public static void B()
 		{
+			var ab = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			var a = ab[0];
+			var b = ab[1];
 
+			var dict = new Dictionary<int, char>();
+			for (var i = 0; i < 10; ++i) {
+				dict.Add(i, 'x');
+			}
+
+			if (a > 0) {
+				var p = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+				foreach (var item in p) {
+					dict[item] = '.';
+				}
+			} else {
+				Console.ReadLine();
+			}
+
+			if (b > 0) {
+				var q = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+				foreach (var item in q) {
+					dict[item] = 'o';
+				}
+			} else {
+				Console.ReadLine();
+			}
+
+			Console.Write($"{dict[7]} {dict[8]} {dict[9]} {dict[0]}");
+			Console.WriteLine($"");
+			Console.Write($" {dict[4]} {dict[5]} {dict[6]}");
+			Console.WriteLine($"");
+			Console.Write($"  {dict[2]} {dict[3]}");
+			Console.WriteLine($"");
+			Console.Write($"   {dict[1]}");
+			Console.WriteLine($"");
+
+			Console.ReadKey();
 		}
 
 		public static void C()
