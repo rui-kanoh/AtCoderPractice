b10f6db730c896fb71b894d59d79de4c618eeebd
rui.kanoh <rui.kanoh@gmail.com>
2020-07-02 10:38:00 +0900

    DotNetCore

---
+++ AtCoderDotNetCore/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace AtCoderDotNetCore
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Question.Exec();
+		}
+	}
+
+	public static class Question
+	{
+		public static void Exec()
+		{
+			double x = double.Parse(Console.ReadLine());
+			long n = (long)Math.Sqrt(Math.Sqrt(x));
+			Console.WriteLine($"{n}");
+		}
+	}
+}
+
+namespace AtCoderDotNetCore
+{
+	public class Template
+	{
+		public static void Exec()
+		{
+			var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+			Console.SetOut(sw);
+
+			string s = Console.ReadLine();
+
+			long ln = long.Parse(Console.ReadLine());
+			long n = int.Parse(Console.ReadLine());
+
+			string[] inputStrArray = Console.ReadLine().Split(' ');
+
+			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			var larray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+			string result = "";
+
+			Console.WriteLine(result);
+
+			Console.Out.Flush();
+
+			Console.ReadKey();
+		}
+
+		public static void A()
+		{
+		}
+
+		public static void B()
+		{
+		}
+
+		public static void C()
+		{
+		}
+
+		public static void D()
+		{
+
+		}
+
+		public static void E()
+		{
+
+		}
+
+		public static void F()
+		{
+
+		}
+
+		public static void G()
+		{
+
+		}
+
+		public static void H()
+		{
+
+		}
+
+		public static void I()
+		{
+
+		}
+
+		public static void J()
+		{
+
+		}
+
+		public static bool IsOdd(long n)
+		{
+			bool isOdd = n % 2 == 1;
+			return isOdd;
+		}
+
+		public static long Gcd(long a, long b)
+		{
+			if (b == 0) {
+				return a;
+			}
+
+			return Gcd(b, a % b);
+		}
+
+		public static long Lcm(long a, long b)
+		{
+			long g = Gcd(a, b);
+			return a / g * b;
+		}
+	}
+}
+
