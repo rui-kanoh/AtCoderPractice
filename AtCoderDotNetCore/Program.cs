c68dc723e9d43e41cbf20b00d49937e0ded8383c
rui.kanoh <rui.kanoh@gmail.com>
2020-08-26 12:56:23 +0900

    Cleanup

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -21,6 +21,7 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+
 		}
 	}
 }
@@ -55,92 +56,22 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			Dictionary<char, char> dict = new Dictionary<char, char>();
-			dict.Add('A', 'T');
-			dict.Add('C', 'G');
-			dict.Add('G', 'C');
-			dict.Add('T', 'A');
-			char c = Console.ReadLine()[0];
 
-			Console.WriteLine($"{dict[c]}");
 		}
 
 		public static void B()
 		{
-			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
-			int k = array[0];
-			int x = array[1];
 
-			for (var i = x - k + 1; i < x + k; ++i) {
-				Console.Write($"{i}");
-				if (i < x + k - 1) {
-					Console.Write($" ");
-				}
-			}
-
-			Console.WriteLine($"");
 		}
 
 		public static void C()
 		{
-			var a = new int[3, 3];
-			var dict = new Dictionary<(int, int), bool>();
-			for (var j = 0; j < 3; ++j) {
-				var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
-				for (var k = 0; k < 3; ++k) {
-					a[j, k] = array[k];
-					dict.Add((j, k), false);
-				}
-			}
 
-			long n = int.Parse(Console.ReadLine());
-			var b = new int[n];
-			for (var j = 0; j < n; ++j) {
-				b[j] = int.Parse(Console.ReadLine());
-			}
-
-			for (var j = 0; j < 3; ++j) {
-				for (var k = 0; k < 3; ++k) {
-					for (var l = 0; l < n; ++l) {
-						if (b[l] == a[j, k]) {
-							dict[(j, k)] = true;
-						}
-					}
-				}
-			}
-
-			bool bingo = dict[(0, 0)] && dict[(0, 1)] && dict[(0, 2)];
-			bingo = bingo || (dict[(1, 0)] && dict[(1, 1)] && dict[(1, 2)]);
-			bingo = bingo || (dict[(2, 0)] && dict[(2, 1)] && dict[(2, 2)]);
-			bingo = bingo || (dict[(0, 0)] && dict[(1, 0)] && dict[(2, 0)]);
-			bingo = bingo || (dict[(0, 1)] && dict[(1, 1)] && dict[(2, 1)]);
-			bingo = bingo || (dict[(0, 2)] && dict[(1, 2)] && dict[(2, 2)]);
-			bingo = bingo || (dict[(0, 0)] && dict[(1, 1)] && dict[(2, 2)]);
-			bingo = bingo || (dict[(0, 2)] && dict[(1, 1)] && dict[(2, 0)]);
-
-			if (bingo) {
-				Console.WriteLine("Yes");
-			} else {
-				Console.WriteLine("No");
-			}
 		}
 
 		public static void D()
 		{
-			long deno = 1;
-			for (int i = 0; i < 9; ++i) {
-				deno *= 10;
-			}
-			deno += 7;
 
-			long n = long.Parse(Console.ReadLine());
-			long power = 1;
-			for (long i = 1; i <= n; ++i) {
-				power *= i;
-				power = power % deno;
-			}
-
-			Console.WriteLine($"{power}");
 		}
 
 		public static void E()
