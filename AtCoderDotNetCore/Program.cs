b7ea88372551237a30e9fbe12c147879c40e5c5f
rui.kanoh <rui.kanoh@gmail.com>
2020-09-09 13:59:36 +0900

    4問目TLE

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -19,9 +19,44 @@ namespace AtCoderDotNetCore
 
 	public static class Question
 	{
+		public static bool IsPrime(int n)
+		{
+			if (n <= 1) {
+				return false;
+			}
+
+			bool isPrime = true;
+			for (var i = 2; i < n; ++i) {
+				if (n % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+
+			return isPrime;
+		}
+
 		public static void Exec()
 		{
+			long q = int.Parse(Console.ReadLine());
+			var counts = new int[q];
+			for (var i = 0; i < q; ++i) {
+				var lr = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+				int l = lr[0];
+				int r = lr[1];
+				for (var j = l; j <= r; j = j + 2) {
+					if (IsPrime(j)) {
+						int jj = (j + 1) / 2;
+						if (IsPrime(jj)) {
+							++counts[i];
+						}
+					}
+				}
+			}
 
+			for (var i = 0; i < q; ++i) {
+				Console.WriteLine($"{counts[i]}");
+			}
 		}
 	}
 }
@@ -56,22 +91,78 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-
+			var n = int.Parse(Console.ReadLine());
+			if (n % 2 == 0) {
+				Console.WriteLine("Blue");
+			} else {
+				Console.WriteLine("Red");
+			}
 		}
 
 		public static void B()
 		{
+			var n = int.Parse(Console.ReadLine());
+			var t1 = "TAKAHASHIKUN";
+			var t2 = "Takahashikun";
+			var t3 = "takahashikun";
+			int count = 0;
+			string[] inputStrArray = Console.ReadLine().Split(' ');
+			for (var i = 0; i < n; ++i) {
+				string str = inputStrArray[i];
+				if (i == n - 1) {
+					str = str.Trim('.');
+				}
 
+				if (str == t1
+					|| str == t2
+					|| str == t3) {
+					++count;
+				}
+			}
+
+			Console.WriteLine($"{count}");
 		}
 
 		public static void C()
 		{
+			var xy = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+			long x = xy[0];
+			long y = xy[1];
+			long a = x;
+			var count = 1;
+			for (var i = 1; i < y / x; ++i) {
+				a *= 2;
+				if (a > y) {
+					break;
+				}
 
+				++count;
+			}
+
+			Console.WriteLine($"{count}");
 		}
 
 		public static void D()
 		{
+			long q = int.Parse(Console.ReadLine());
+			var counts = new int[q];
+			for (var i = 0; i < q; ++i) {
+				var lr = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+				int l = lr[0];
+				int r = lr[1];
+				for (var j = l; j <= r; j = j + 2) {
+					if (IsPrime(j)) {
+						int jj = (j + 1) / 2;
+						if (IsPrime(jj)) {
+							++counts[i];
+						}
+					}
+				}
+			}
 
+			for (var i = 0; i < q; ++i) {
+				Console.WriteLine($"{counts[i]}");
+			}
 		}
 
 		public static void E()
