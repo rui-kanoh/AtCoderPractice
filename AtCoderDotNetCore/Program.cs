aa1afa8bcc4b24ed7288cd65bc720187946b80ce
elionix-kanoh <272-kanoh@elionix.co.jp>
2020-12-16 16:47:27 +0900

    3問目まで

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -22,7 +22,39 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+			var n = array[0];
+			var m = array[1];
+			var c = new long[n];
+			for (var i = 0; i < n; ++i) {
+				c[i] = long.Parse(Console.ReadLine());
+			}
 
+			long power = Math.Abs(m - c[0]);
+			var hash = new HashSet<long>();
+			for (var i = 1; i < n; ++i) {
+				var val2 = Math.Abs(m - c[i]);
+				if (val2 == 0) {
+					continue;
+				} else {
+					hash.Add(val2);
+				}
+
+				for (var j = 0; j < i; ++j) {
+					var val = Math.Abs(c[j] - c[i]);
+					if (hash.Contains(val)) {
+						break;
+					} else {
+						hash.Add(val);
+					}
+				}
+
+				power += hash.Min();
+
+				hash.Clear();
+			}
+
+			Console.WriteLine($"{power}");
 		}
 	}
 }
@@ -53,16 +85,66 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			var nm = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			int n = nm[0];
+			int m = nm[1];
 
+			int answer = (n - 1) * (m - 1);
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
+			string s = Console.ReadLine();
+			var abcd = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			string d = "\"";
+			if (abcd[0] == 0) {
+				s = d + s;
+			} else {
+				s = s.Insert(abcd[0], d);
+			}
 
+			for (var i = 1; i < 4; ++i) {
+				s = s.Insert(abcd[i] + i, d);
+			}
+
+			Console.WriteLine(s);
+		}
+
+		public static void C()
+		{
+			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+			var a = array[0];
+			var b = array[1];
+			var k = array[2];
+			var l = array[3];
+
+			long min = long.MaxValue;
+			for (var i = 0; i <= k; ++i) {
+				long aa = a * i;
+				long bb = b * (k / l);
+
+				long count = aa + bb;
+				if (k % l != 0) {
+					if (b > a * (k % l)) {
+						count += a * (k % l);
+					} else {
+						count += b;
+					}
+				}
+
+				if (min > count) {
+					min = count;
+				}
+			}
+
+			long answer = min;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void D()
 		{
+			//グラフなのでパス
 		}
 
 		public static void E()
