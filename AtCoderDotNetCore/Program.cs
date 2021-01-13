6981af18505f72ed84d07b9a392cb5a0a20bbf3e
rui.kanoh <rui.kanoh@gmail.com>
2021-01-13 15:01:25 +0900

    3問目まで。

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -22,6 +22,57 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			int n = array[0];
+			int k = array[1];
+			var dlist = new List<long>();
+			for (int i = 0; i < n; ++i) {
+				dlist.Add(long.Parse(Console.ReadLine()));
+			}
+
+			var xydict = new Dictionary<int, List<int>>();
+			for (int i = 0; i < k; ++i) {
+				var xy = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+				if (xydict.ContainsKey(xy[0]) == false) {
+					xydict.Add(xy[0], new List<int>() { xy[1] });
+				} else {
+					xydict[xy[0]].Add(xy[1]);
+				}
+
+				if (xydict.ContainsKey(xy[1]) == false) {
+					xydict.Add(xy[1], new List<int>() { xy[0] });
+				} else {
+					xydict[xy[1]].Add(xy[0]);
+				}
+			}
+
+			static void CalculatePoint(int start, ref long ans, Dictionary<int, List<int>> dict, List<long> list, ref int count, int max)
+			{
+				ans += list[start];
+				++count;
+				if (dict.ContainsKey(start) == false
+					|| count >= max) {
+					return;
+				}
+
+				start = dict[start];
+				CalculatePoint(start, ref ans, dict, list, ref count, max);
+			}
+
+			long max = 0;
+
+			for (int i = 0; i < n; ++i) {
+				long answer = 0;
+				int count = 0;
+
+				CalculatePoint(i, ref answer, xydict, dlist, ref count, n);
+				if (answer > max) {
+					max = answer;
+				}
+			}
+
+			Console.WriteLine($"{max}");
 		}
 	}
 }
@@ -52,66 +103,70 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			var nm = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
-			int n = nm[0];
-			int m = nm[1];
+			var array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+			int h = array[0];
+			int w = array[1];
+			int answer = 0;
 
-			int answer = (n - 1) * (m - 1);
+			if (h == 1 && w == 1) {
+				answer = 0;
+			} else if (h == 1 || w == 1) {
+				answer = h == 1 ? w - 1 : h - 1;
+			} else {
+				answer = (h - 1) * w + h * (w - 1);
+			}
+
 			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
-			string s = Console.ReadLine();
-			var abcd = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
-			string d = "\"";
-			if (abcd[0] == 0) {
-				s = d + s;
-			} else {
-				s = s.Insert(abcd[0], d);
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			int c = int.Parse(Console.ReadLine());
+
+			var dict = new Dictionary<int, int>();
+			if (a > b && b > c) {
+				dict.Add(a, 1);
+				dict.Add(b, 2);
+				dict.Add(c, 3);
+			} else if (a > b && b < c && c < a) {
+				dict.Add(a, 1);
+				dict.Add(b, 3);
+				dict.Add(c, 2);
+			} else if (a < b && b > c && c < a) {
+				dict.Add(a, 2);
+				dict.Add(b, 1);
+				dict.Add(c, 3);
+			} else if (c > a && a > b) {
+				dict.Add(a, 2);
+				dict.Add(b, 3);
+				dict.Add(c, 1);
+			} else if (a < c && c < b) {
+				dict.Add(a, 3);
+				dict.Add(b, 1);
+				dict.Add(c, 2);
+			} else if (a < c && b < c && a < b) {
+				dict.Add(a, 3);
+				dict.Add(b, 2);
+				dict.Add(c, 1);
 			}
 
-			for (var i = 1; i < 4; ++i) {
-				s = s.Insert(abcd[i] + i, d);
-			}
 
-			Console.WriteLine(s);
+			Console.WriteLine($"{dict[a]}");
+			Console.WriteLine($"{dict[b]}");
+			Console.WriteLine($"{dict[c]}");
 		}
 
 		public static void C()
 		{
-			var array = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
-			var a = array[0];
-			var b = array[1];
-			var k = array[2];
-			var l = array[3];
-
-			long min = long.MaxValue;
-			for (var i = 0; i <= k; ++i) {
-				long aa = a * i;
-				long bb = b * (k / l);
-
-				long count = aa + bb;
-				if (k % l != 0) {
-					if (b > a * (k % l)) {
-						count += a * (k % l);
-					} else {
-						count += b;
-					}
-				}
-
-				if (min > count) {
-					min = count;
-				}
-			}
-
-			long answer = min;
+			int answer = 654231;
 			Console.WriteLine($"{answer}");
 		}
 
 		public static void D()
 		{
-			//グラフなのでパス
+
 		}
 
 		public static void E()
