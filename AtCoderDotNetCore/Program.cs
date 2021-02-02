bf5373c87ccb1c4adaf65154db3343bf022941ad
rui.kanoh <rui.kanoh@gmail.com>
2021-02-03 08:52:45 +0900

    cleanup

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -23,40 +23,8 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
-			var np = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			long n = np[0];
-			long p = np[1];
-			var a = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-
-			long temp = 1;
-			int r = 0;
-			for (int l = 0; l < n;) {
-				while (r < n && temp < p) {
-					temp *= a[r];
-					++r;
-				}
-
-				if (temp == p) {
-					Console.WriteLine("Yay!");
-					return;
-				}
-
-				while (l < r && temp > p) {
-					temp /= a[l];
-					++l;
-				}
-
-				if (temp == p) {
-					Console.WriteLine("Yay!");
-					return;
-				}
-
-				if (r == n) {
-					break;
-				}
-			}
-
-			Console.WriteLine(":(");
+			int answer = 0;
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -87,40 +55,11 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			int n = int.Parse(Console.ReadLine());
-			var array = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-
-			int answer = 0;
-			bool canDivide = true;
-			while (canDivide) {
-				for (var i = 0; i < array.Length; ++i) {
-					if (IsOdd(array[i])) {
-						canDivide = false;
-						break;
-					}
-
-					array[i] /= 2;
-				}
-
-				if (canDivide) {
-					++answer;
-				}
-			}
-
-			Console.WriteLine($"{answer}");
+			
 		}
 
 		public static void B()
 		{
-			string str = Console.ReadLine();
-			var deleteChars = new[] { "a", "i", "u", "e", "o" };
-
-			string answer = str;
-			for (var i = 0; i < deleteChars.Length; ++i) {
-				answer = answer.Replace(deleteChars[i], "");
-			}
-
-			Console.WriteLine($"{answer}");
 		}
 
 		public static void C()
