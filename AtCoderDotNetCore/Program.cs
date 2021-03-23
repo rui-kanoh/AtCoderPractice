3618a0de3c9b13358254e6d20a258e68454188e5
elionix-kanoh <272-kanoh@elionix.co.jp>
2021-03-24 08:55:59 +0900

    cleanup

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -41,7 +41,7 @@ namespace AtCoderDotNetCore
 
 			string[] inputStrArray = Console.ReadLine().Split(" ");
 
-			var array = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
 			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
 
 			string result = "";
@@ -56,13 +56,111 @@ namespace AtCoderDotNetCore
 			var a = array[1];
 			var b = array[2];
 			if (Math.Abs(x - a) < Math.Abs(x - b)) {
-				Console.WriteLine("A"); ;
+				Console.WriteLine("A");
 			} else {
-				Console.WriteLine("B"); ;
+				Console.WriteLine("B");
 			}
 		}
 
 		public static void B()
+		{
+
+		}
+
+		public static void C()
+		{
+
+		}
+
+		public static void D()
+		{
+
+		}
+
+		public static void E()
+		{
+
+		}
+
+		public static bool IsOdd(long n)
+		{
+			bool isOdd = n % 2 == 1;
+			return isOdd;
+		}
+
+		public static long Gcd(long a, long b)
+		{
+			if (b == 0) {
+				return a;
+			}
+
+			return Gcd(b, a % b);
+		}
+
+		public static long Lcm(long a, long b)
+		{
+			long g = Gcd(a, b);
+			return a / g * b;
+		}
+
+		public static void DfsSample(List<string> items, int num)
+		{
+			if (items.Count == num) {
+
+				foreach (var item in items) {
+					Console.Write($"{item} ");
+				}
+				Console.WriteLine("");
+
+				return;
+			}
+
+			for (var i = 0; i <= (int)('d' - 'a'); ++i) {
+				items.Add($"{(char)('a' + i)}");
+				DfsSample(items, num);
+				items.RemoveAt(items.Count - 1);
+			}
+		}
+
+		public static void SaitoDfs()
+		{
+			var nk = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nk[0];
+			var k = nk[1];
+			var a = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+
+			long ans = long.MaxValue;
+
+			void Dfs(List<long> items, long last)
+			{
+				if (items.Count == k) {
+					long count = 0;
+
+					// 計算ロジック
+
+					if (ans > count) {
+						ans = count;
+					}
+
+					return;
+				}
+
+				// 重複を防ぐために自分のindexより大きいものだけを選ぶ
+				long start = last + 1;
+
+				for (long i = start; i < n; i++) {
+					items.Add(i);
+					Dfs(items, i);
+					items.RemoveAt(items.Count - 1);
+				}
+			}
+
+			Dfs(new List<long>(), -1);
+
+			Console.WriteLine($"{ans}");
+		}
+
+		public static void TerabunDFS()
 		{
 			int num = 10;
 			var map = new bool[num, num];
@@ -167,7 +265,8 @@ namespace AtCoderDotNetCore
 			}
 		}
 
-		public static void B2 (){
+		public static void SaitoBSF()
+		{
 			int n = 10;
 			var a = new bool[n, n];
 			for (int i = 0; i < n; i++) {
@@ -247,281 +346,6 @@ namespace AtCoderDotNetCore
 			}
 
 			Console.WriteLine(ok ? "YES" : "NO");
-		}
-
-		public static void C()
-		{
-			int K = int.Parse(Console.ReadLine());
-
-			int CalcCount(int d, int K)
-			{
-				int r = d / 2;
-				int rr = r * r;
-				int blockCount = d / K;
-
-				var lists = new List<List<bool>>();
-				for (var i = 0; i <= blockCount; ++i) {
-					var list = new List<bool>();
-					for (var j = 0; j <= blockCount; ++j) {
-						long distX = Math.Abs(K * i - r);
-						long distY = Math.Abs(K * j - r);
-						long dist = distX * distX + distY * distY;
-						bool inCircle = false;
-						if (dist <= rr) {
-							inCircle = true;
-						}
-						list.Add(inCircle);
-					}
-					lists.Add(list);
-				}
-
-				int count = 0;
-				for (var i = 0; i < blockCount; ++i) {
-					for (var j = 0; j < blockCount; ++j) {
-						if (lists[i][j]
-							&& lists[i + 1][j]
-							&& lists[i][j + 1]
-							&& lists[i + 1][j + 1]
-							&& lists[i + 1][j + 1]) {
-							++count;
-						}
-					}
-				}
-
-				//int count = list.Count(x => x != false);
-				//int sqCount = (int)Math.Sqrt(count) - 1;
-				//sqCount = sqCount * sqCount;
-				return count;
-			}
-
-			int sqCount200 = CalcCount(200, K);
-			int sqCount300 = CalcCount(300, K);
-			Console.WriteLine($"{sqCount200} {sqCount300}");
-		}
-
-		public static void D()
-		{
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			int n = array[0];
-			var dict = new Dictionary<string, int>();
-			dict.Add("a", array[1]);
-			dict.Add("b", array[2]);
-			dict.Add("c", array[3]);
-
-			var listL = new List<int>();
-			for (var i = 0; i < n; ++i) {
-				listL.Add(int.Parse(Console.ReadLine()));
-			}
-
-			int min = int.MaxValue;
-
-			void Dfs(List<string> items, int num)
-			{
-				if (items.Count == num) {
-					var sumA = 0;
-					var sumB = 0;
-					var sumC = 0;
-					var countA = -1;
-					var countB = -1;
-					var countC = -1;
-					for (var i = 0; i < items.Count; ++i) {
-						if (items[i] == "a") {
-							sumA += listL[i];
-							++countA;
-						} else if (items[i] == "b") {
-							sumB += listL[i];
-							++countB;
-						} else if (items[i] == "c") {
-							sumC += listL[i];
-							++countC;
-						}
-					}
-
-					if (countA == -1 || countB == -1 || countC == -1) {
-						return;
-					}
-
-					int answer = (countA + countB + countC) * 10
-						+ Math.Abs(dict["a"] - sumA)
-						+ Math.Abs(dict["b"] - sumB)
-						+ Math.Abs(dict["c"] - sumC);
-					if (min > answer) {
-						min = answer;
-					}
-
-					return;
-				}
-
-				for (var i = 0; i <= (int)('d' - 'a'); ++i) {
-					items.Add($"{(char)('a' + i)}");
-					Dfs(items, num);
-					items.RemoveAt(items.Count - 1);
-				}
-			}
-
-			Dfs(new List<string>(), n);
-
-			Console.WriteLine($"{min}");
-		}
-
-		public static void E()
-		{
-			//int n = int.Parse(Console.ReadLine());
-
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			int n = array[0];
-			var dict = new Dictionary<string, int>();
-			dict.Add("a", array[1]);
-			dict.Add("b", array[2]);
-			dict.Add("c", array[3]);
-
-			var listL = new List<int>();
-			for (var i = 0; i < n; ++i) {
-				listL.Add(int.Parse(Console.ReadLine()));
-			}
-
-			int min = int.MaxValue;
-
-			void Dfs(List<string> items, int num)
-			{
-				if (items.Count == num) {
-					/*
-					foreach (var item in items) {
-						Console.Write($"{item} ");
-					}
-					Console.WriteLine("");
-					*/
-
-					var sumA = 0;
-					var sumB = 0;
-					var sumC = 0;
-					var countA = -1;
-					var countB = -1;
-					var countC = -1;
-					for (var i = 0; i < items.Count; ++i) {
-						if (items[i] == "a") {
-							sumA += listL[i];
-							++countA;
-						} else if (items[i] == "b") {
-							sumB += listL[i];
-							++countB;
-						} else if (items[i] == "c") {
-							sumC += listL[i];
-							++countC;
-						}
-					}
-
-					if (countA == -1 || countB == -1 || countC == -1) {
-						return;
-					}
-
-					/*
-					foreach (var item in items) {
-						Console.Write($"{item} ");
-					}
-					Console.WriteLine("");
-					*/
-
-					int answer = (countA + countB + countC) * 10
-						+ Math.Abs(dict["a"] - sumA)
-						+ Math.Abs(dict["b"] - sumB)
-						+ Math.Abs(dict["c"] - sumC);
-					//Console.WriteLine($"{answer}");
-					if (min > answer) {
-						min = answer;
-					}
-
-					return;
-				}
-
-				for (var i = 0; i <= (int)('d' - 'a'); ++i) {
-					items.Add($"{(char)('a' + i)}");
-					Dfs(items, num);
-					items.RemoveAt(items.Count - 1);
-				}
-			}
-
-			Dfs(new List<string>(), n);
-
-			Console.WriteLine($"{min}");
-		}
-
-		public static bool IsOdd(long n)
-		{
-			bool isOdd = n % 2 == 1;
-			return isOdd;
-		}
-
-		public static long Gcd(long a, long b)
-		{
-			if (b == 0) {
-				return a;
-			}
-
-			return Gcd(b, a % b);
-		}
-
-		public static long Lcm(long a, long b)
-		{
-			long g = Gcd(a, b);
-			return a / g * b;
-		}
-
-		public static void DfsSample(List<string> items, int num)
-		{
-			if (items.Count == num) {
-
-				foreach (var item in items) {
-					Console.Write($"{item} ");
-				}
-				Console.WriteLine("");
-
-				return;
-			}
-
-			for (var i = 0; i <= (int)('d' - 'a'); ++i) {
-				items.Add($"{(char)('a' + i)}");
-				DfsSample(items, num);
-				items.RemoveAt(items.Count - 1);
-			}
-		}
-
-		public static void SaitoDfs()
-		{
-			var nk = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var n = nk[0];
-			var k = nk[1];
-			var a = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-
-			long ans = long.MaxValue;
-
-			void Dfs(List<long> items, long last)
-			{
-				if (items.Count == k) {
-					long count = 0;
-
-					// 計算ロジック
-
-					if (ans > count) {
-						ans = count;
-					}
-
-					return;
-				}
-
-				// 重複を防ぐために自分のindexより大きいものだけを選ぶ
-				long start = last + 1;
-
-				for (long i = start; i < n; i++) {
-					items.Add(i);
-					Dfs(items, i);
-					items.RemoveAt(items.Count - 1);
-				}
-			}
-
-			Dfs(new List<long>(), -1);
-
-			Console.WriteLine($"{ans}");
 		}
 	}
 }
