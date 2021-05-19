1224bf9e092e599f37aafc133b814c0577b8cf32
elionix-kanoh <272-kanoh@elionix.co.jp>
2021-05-19 09:38:04 +0900

    move

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -207,12 +207,12 @@ namespace AtCoderDotNetCore
 
 		public static void D()
 		{
-			
+
 		}
 
 		public static void E()
 		{
-			
+
 		}
 
 		public static (bool isFound, int left, int right) BinarySearch(int value, List<int> list)
@@ -640,6 +640,72 @@ namespace AtCoderDotNetCore
 			Dfs(new List<int>(), 2, -1);
 
 			double answer = Math.Round(Math.Sqrt(answer2), 4);
+			Console.WriteLine($"{answer}");
+		}
+
+		public class Holiday
+		{
+			public bool isHoliday = true;
+			public bool usesPaid = false;
+		}
+
+		public static void Syakutori()
+		{
+			var nk = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nk[0];
+			var k = nk[1];
+			var h = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var holidays = new Holiday[n];
+			for (var i = 0; i < n; ++i) {
+				holidays[i] = new Holiday();
+				holidays[i].isHoliday = h[i] == 1 ? true : false;
+			}
+
+			int max = 0;
+			int end = 0;
+
+			int sum = 0;
+			int paid = k;
+			for (int start = 0; start < n; ++start) {
+				// paidが0になるまでendを伸ばす
+				while (end < n) {
+					if (holidays[end].isHoliday) {
+						++sum;
+					} else {
+						if (paid > 0) {
+							--paid;
+							holidays[end].usesPaid = true;
+							++sum;
+						} else {
+							break;
+						}
+					}
+
+					++end;
+				}
+
+				max = Math.Max(max, sum);
+				//Console.WriteLine($"{start} {end} {paid} {sum} {max}");
+
+				if (end == start) {
+					++end;
+				}
+
+				if (holidays[start].isHoliday) {
+					--sum;
+				} else {
+					if (holidays[start].usesPaid) {
+						holidays[start].usesPaid = false;
+						++paid;
+						--sum;
+					}
+				}
+
+				//Console.WriteLine($"{start} {end} {paid} {sum}");
+			}
+
+			var answer = max;
+
 			Console.WriteLine($"{answer}");
 		}
 	}
