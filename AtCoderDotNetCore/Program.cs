ee7e9cb953e5f1dc7b6859027fe77c64aa6c04f6
elionix-kanoh <272-kanoh@elionix.co.jp>
2021-08-25 09:16:36 +0900

    TLE

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -23,9 +23,48 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
-			long answer = 0;
+			var hw = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var h = hw[0];
+			var w = hw[1];
+			var mat = new int[h, w];
+			for (var i = 0; i < h; ++i) {
+				var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				for (var j = 0; j < a.Length; ++j) {
+					mat[i, j] = a[j];
+				}
+			}
 
-			Console.WriteLine($"{answer}");
+			var rList = new List<int>();
+			for (var i = 0; i < h; ++i) {
+				int sum = 0;
+				for (var j = 0; j < w; ++j) {
+					sum += mat[i, j];
+				}
+
+				rList.Add(sum);
+			}
+
+			var cList = new List<int>();
+			for (var i = 0; i < w; ++i) {
+				int sum = 0;
+				for (var j = 0; j < h; ++j) {
+					sum += mat[j, i];
+				}
+
+				cList.Add(sum);
+			}
+
+			var matB = new int[h, w];
+			for (var i = 0; i < h; ++i) {
+				for (var j = 0; j < w; ++j) {
+					matB[i, j] = rList[i] + cList[j] - mat[i, j];
+					if (j < w - 1) {
+						Console.Write($"{matB[i, j]} ");
+					} else {
+						Console.WriteLine($"{matB[i, j]}");
+					}
+				}
+			}
 		}
 	}
 }
@@ -53,7 +92,14 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			string s = Console.ReadLine();
+			var list = s.ToCharArray().ToList();
+			int sindex = list.IndexOf('A');
+			list.Reverse();
+			int eindex = list.Count - list.IndexOf('Z');
 
+			int answer = eindex - sindex;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
@@ -75,7 +121,6 @@ namespace AtCoderDotNetCore
 		{
 
 		}
-
 	}
 }
 
