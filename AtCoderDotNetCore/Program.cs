fcffce9f6d5da4c98973d4a7b295311d828581c9
elionix-kanoh <272-kanoh@elionix.co.jp>
2021-09-01 10:04:30 +0900

    2問目までなら何とか解けた

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -23,7 +23,85 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			int n = int.Parse(Console.ReadLine());
 
+			var list1 = new List<(long point, int number)>();
+
+			var list2 = new List<(long point, int number)>();
+
+			for (var i = 0; i < n; ++i) {
+				var cp = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				if (cp[0] == 1) {
+					list1.Add((cp[1], i));
+					list2.Add((0, i));
+				} else {
+					list2.Add((cp[1], i));
+					list1.Add((0, i));
+				}
+			}
+
+			var listRui1 = new List<long>();
+			listRui1.Add(list1[0].point);
+			for (var i = 1; i < n; ++i) {
+				if (list1[i].number != i) {
+					listRui1.Add(listRui1[i - 1]);
+				} else {
+					listRui1.Add((list1[i].point + listRui1[i - 1]));
+				}
+			}
+
+			/*
+			var index = 0;
+			foreach (var item in listRui1) {
+				Console.WriteLine($"{index} {item}");
+				++index;
+			}
+			*/
+
+			var listRui2 = new List<long>();
+			listRui2.Add(list2[0].point);
+			for (var i = 1; i < list2.Count; ++i) {
+				if (list2[i].number < i) {
+					listRui2.Add(listRui2[i - 1]);
+				} else {
+					listRui2.Add((list2[i].point + listRui2[i - 1]));
+				}
+			}
+
+			/*
+			index = 0;
+			foreach (var item in listRui2) {
+				Console.WriteLine($"{index} {item}");
+				++index;
+			}
+			*/
+
+			/*
+			var listRui2 = new List<(long point, int number)>();
+			listRui2.Add(list2[0]);
+			for (var i = 1; i < list2.Count; ++i) {
+				listRui2.Add((list2[i].point + listRui2[i - 1].point, list2[i].number));
+			}
+			*/
+
+			var lrList = new List<(int l, int r)>();
+			int q = int.Parse(Console.ReadLine());
+			for (var i = 0; i < q; ++i) {
+				var lr = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				lrList.Add((lr[0] - 1, lr[1] - 1));
+			}
+
+			for (var i = 0; i < q; ++i) {
+				//long a = listRui1[dict1[lrList[i].r]].point - listRui1[dict1[lrList[i].l] > 0 ? dict1[lrList[i].l] - 1 : 0].point;
+				//long b = listRui2[dict2[lrList[i].r]].point - listRui2[dict2[lrList[i].l] > 0 ? dict2[lrList[i].l] - 1 : 0].point;
+				int indexAr = lrList[i].r >= listRui1.Count ? listRui1.Count - 1 : lrList[i].r;
+				long al = lrList[i].l > 0 ? listRui1[lrList[i].l - 1] : 0;
+				long a = listRui1[indexAr] - al;
+				int indexBr = lrList[i].r >= listRui2.Count ? listRui2.Count - 1 : lrList[i].r;
+				long bl = lrList[i].l > 0 ? listRui2[lrList[i].l - 1] : 0;
+				long b = listRui2[indexBr] - bl;
+				Console.WriteLine($"{a} {b}");
+			}
 		}
 	}
 }
@@ -51,7 +129,9 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-
+			int n = int.Parse(Console.ReadLine());
+			var answer = n >= 1200 ? "ARC" : "ABC";
+			Console.WriteLine(answer);
 		}
 
 		public static void B()
