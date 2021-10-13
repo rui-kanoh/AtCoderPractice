5a4df3c08235fb4ab5feda4555e0ba289d5c64b3
elionix-kanoh <272-kanoh@elionix.com>
2021-10-13 12:19:22 +0900

    3問目4つWrongAnswer

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -23,7 +23,51 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
-			
+			var nq = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nq[0];
+			var q = nq[1];
+			var a = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var diffs = new long[a.Length - 1];
+			for (var i = 0; i < diffs.Length - 1; ++i) {
+				diffs[i] = a[i] - a[i + 1];
+			}
+
+			long Calc(long[] e)
+			{
+				long value = 0;
+				for (var i = 0; i < e.Length - 1; ++i) {
+					value += Math.Abs(e[i] - e[i + 1]);
+				}
+
+				return value;
+			}
+
+			var total = Calc(a);
+
+			var builder = new StringBuilder();
+			for (var i = 0; i < q; ++i) {
+				var lrv = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				var l = lrv[0] - 1;
+				var r = lrv[1] - 1;
+				var v = lrv[2];
+
+				if (l != r) {
+					if (l > 0) {
+						total += (-Math.Abs(diffs[l - 1]) + Math.Abs(diffs[l - 1] - v));
+						diffs[l - 1] -= v;
+					}
+
+					if (r < a.Length - 1) {
+						total += (-Math.Abs(diffs[r]) + Math.Abs(diffs[r] + v));
+						diffs[r] += v;
+					}
+				}
+
+				builder.Append($"{total}" + Environment.NewLine);
+			}
+
+			var answer = builder.ToString();
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -32,6 +76,12 @@ namespace AtCoderDotNetCore
 {
 	public class Template
 	{
+		public static bool IsOdd(long n)
+		{
+			bool isOdd = (n & 0x1) == 0x1;
+			return isOdd;
+		}
+
 		public static void Exec()
 		{
 			string s = Console.ReadLine();
@@ -51,12 +101,39 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			int n = int.Parse(Console.ReadLine());
 
+			long value = 10000;
+			long total = 0;
+			for (var i = 1; i <= n; ++i) {
+				total += value * i;
+			}
+
+			long answer = total / n;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
+			var nk = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nk[0];
+			var k = nk[1];
+			var a = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var b = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			bool isOK = false;
+			long diff = 0;
+			for (var i = 0; i < n; ++i) {
+				diff += Math.Abs(a[i] - b[i]);
+			}
 
+			if (diff <= k) {
+				if (IsOdd(diff) == IsOdd(k)) {
+					isOK = true;
+				}
+			}
+
+			var answer = isOK ? "Yes" : "No";
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void C()
