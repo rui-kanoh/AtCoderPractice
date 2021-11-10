aa16369babf7b06be366b2d0ebf44a39505c1d8f
elionix-kanoh <272-kanoh@elionix.com>
2021-11-10 10:10:22 +0900

    時間オーバーしたけど3問目まで溶けた

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -22,82 +22,9 @@ namespace AtCoderDotNetCore
 
 	public static class Question
 	{
-		public static (bool isFound, int left, int right) BinarySearch(long value, List<long> list)
-		{
-			int left = -1;
-			int right = list.Count;
-			while (right - left > 1) {
-				int mid = (right + left) / 2;
-				if (list[mid] == value) {
-					return (true, mid, mid);
-				} else if (list[mid] > value) {
-					right = mid;
-				} else {
-					left = mid;
-				}
-			}
-
-			if (left == -1 || right == list.Count) {
-				return (false, left, right);
-			} else {
-				return (true, left, right);
-			}
-		}
-		/*
-9
-3
-7
-5
-9
-8
-10
-10
-11
-9
-*/
-
 		public static void Exec()
 		{
-			var n = int.Parse(Console.ReadLine());
-			var alist = new List<long>();
-			for (var i = 0; i < n; ++i) {
-				var a = long.Parse(Console.ReadLine());
-				alist.Add(a);
-			}
-
-			var lisTable = Enumerable.Repeat(long.MaxValue, n).ToList();
-			lisTable[0] = 0;
-
-			long length = 0;
-
-			for (var i = 1; i < alist.Count; ++i) {
-				(bool isFound, int left, int right) = BinarySearch(alist[i], lisTable);
-				//Console.WriteLine($"{left},{right}");
-
-				if (isFound) {
-					if (lisTable[left] != long.MaxValue
-						&& lisTable[right] == long.MaxValue) {
-						if (lisTable[left] > alist[i]) {
-							lisTable[left] = alist[i];
-						} else {
-							lisTable[right] = alist[i];
-						}
-					}
-				}
-
-				var limitedList = lisTable.Where(s => s != long.MaxValue).ToList();
-				length = limitedList.Count;
-
-				/*
-				foreach (var item in lisTable) {
-					Console.Write(item == long.MaxValue ? "∞ " : $"{item} ");
-				}
-
-				Console.WriteLine($"");
-				*/
-			}
-
-			Console.WriteLine($"{length}");
+			
 		}
 	}
 }
@@ -131,108 +58,52 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			string[] abc = Console.ReadLine().Split(" ");
-			string title = abc[1];
-			Console.WriteLine($"A{title[0].ToString().ToUpper()}C");
+			var abcd = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var a = abcd[0];
+			var b = abcd[1];
+			var c = abcd[2];
+			var d = abcd[3];
+			var answer = "Balanced";
+			if (a + b < c + d) {
+				answer = "Right";
+			} else if (a + b > c + d) {
+				answer = "Left";
+			}
+
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
-			int n = int.Parse(Console.ReadLine());
-			var t = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			int m = int.Parse(Console.ReadLine());
-			var xplist = new List<(int p, long x)>();
-			for (var i = 0; i < m; ++i) {
-				var px = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-				xplist.Add(((int)px[0] - 1, px[1]));
+			string w = Console.ReadLine();
+
+			bool isBeautiful = true;
+			for (char c = 'a'; c <= 'z'; ++c) {
+				int count = w.Where(s => s == c).Count();
+				if (IsOdd(count)) {
+					isBeautiful = false;
+					break;
+				}
 			}
 
-			long total = t.Sum();
-			var sb = new StringBuilder();
-			for (var i = 0; i < m; ++i) {
-				long answer = total - t[xplist[i].p] + xplist[i].x;
-				sb.Append($"{answer}" + Environment.NewLine);
-			}
-
-			Console.WriteLine(sb.ToString());
+			Console.WriteLine(isBeautiful ? "Yes" : "No");
 		}
 
 		public static void C()
 		{
-			string[] sk = Console.ReadLine().Split(" ");
-			var s = sk[0];
-			var k = int.Parse(sk[1]);
-			var slist = s.ToCharArray();
+			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
 
-			var strList = new List<string>();
+			long time = (100 * (n - m) + 1900 * m) * (1 << m);
 
-			var p = Calc.AllPermutation(Enumerable.Range(0, slist.Length).ToArray());
-
-			foreach (var indexes in p) {
-				var sb = new StringBuilder();
-				//Console.WriteLine(string.Join(" ", item));
-				foreach (var index in indexes) {
-					sb.Append(slist[index]);
-				}
-
-				string str = sb.ToString();
-				strList.Add(str);
-			}
-
-			strList = strList.Distinct().ToList();
-			strList.Sort();
-
-			Console.WriteLine($"{strList[k - 1]}");
+			var answer = time;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void D()
 		{
-			var n = int.Parse(Console.ReadLine());
-			var alist = new List<long>();
-			for (var i = 0; i < n; ++i) {
-				var a = long.Parse(Console.ReadLine());
-				alist.Add(a);
-			}
 
-			long max = 0;
-
-
-			void Dfs(List<int> items, int num, int start)
-			{
-				/*
-				if () {
-					foreach (var item in items) {
-						Console.Write($"{item} ");
-					}
-					Console.WriteLine("");
-
-					max = Math.Max(items.Count, max);
-					return;
-				}
-				*/
-
-				for (var i = start + 1; i < n - 1; ++i) {
-					items.Add(i);
-					if (alist[i] >= alist[i + 1]) {
-
-						foreach (var item in items) {
-							Console.Write($"{item} ");
-						}
-
-						Console.WriteLine("");
-
-						max = Math.Max(items.Count, max);
-						break;
-					}
-
-					Dfs(items, num, i);
-					items.RemoveAt(items.Count - 1);
-				}
-			}
-
-			Dfs(new List<int>(), n, -1);
-
-			Console.WriteLine($"{max}");
 		}
 
 		public static void E()
