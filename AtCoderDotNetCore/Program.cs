06e465942b2cd6672aafa5022306b08f203257e6
elionix-kanoh <272-kanoh@elionix.com>
2021-11-17 12:55:59 +0900

    4問目まで溶けた

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -24,7 +24,9 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			
 		}
+
 	}
 }
 
@@ -32,12 +34,6 @@ namespace AtCoderDotNetCore
 {
 	public class Template
 	{
-		public static bool IsOdd(long n)
-		{
-			bool isOdd = (n & 0x1) == 0x1;
-			return isOdd;
-		}
-
 		public static void Exec()
 		{
 			string s = Console.ReadLine();
@@ -57,77 +53,108 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			var abcd = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var a = abcd[0];
-			var b = abcd[1];
-			var c = abcd[2];
-			var d = abcd[3];
-			var answer = "Balanced";
-			if (a + b < c + d) {
-				answer = "Right";
-			} else if (a + b > c + d) {
-				answer = "Left";
+			long n = long.Parse(Console.ReadLine());
+
+			long keta = (long)Math.Log10(n);
+
+			long value = n;
+			long hardShat = 0;
+			long deno = 1;
+			for (var i = 1; i <= keta + 1; ++i) {
+				deno *= 10;
+				hardShat += (value % deno) / (deno / 10);
 			}
 
-			Console.WriteLine($"{answer}");
+			var answer = n % hardShat == 0;
+
+			Console.WriteLine(answer ? "Yes" : "No");
 		}
 
 		public static void B()
 		{
-			string w = Console.ReadLine();
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
 
-			bool isBeautiful = true;
-			for (char c = 'a'; c <= 'z'; ++c) {
-				int count = w.Where(s => s == c).Count();
-				if (IsOdd(count)) {
-					isBeautiful = false;
-					break;
-				}
+			int min = int.MaxValue;
+
+			int diff = Math.Abs(a - b);
+
+			int value = 0;
+			if (a > b) {
+				value = (9 - a) + b + 1;
+			} else {
+				value = a + (9 - b) + 1;
 			}
 
-			Console.WriteLine(isBeautiful ? "Yes" : "No");
+			min = Math.Min(diff, value);
+
+			var answer = min;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void C()
 		{
-			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
+			int n = int.Parse(Console.ReadLine());
+			var a = new int[n, 6];
+			for (var i = 0; i < n; ++i) {
+				var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				for (var j = 0; j < array.Length; ++j) {
+					a[i, j] = array[j];
+				}
+			}
 
-			long time = (100 * (n - m) + 1900 * m) * (1 << m);
+			BigInteger deno = (BigInteger)(Math.Pow(10, 9) + 7);
 
-			var answer = time;
+			BigInteger mul = 1;
+
+			for (var i = 0; i < n; ++i) {
+				BigInteger sum = 0;
+				for (var j = 0; j < 6; ++j) {
+					sum += a[i, j];
+				}
+
+				mul = (mul * sum) % deno;
+			}
+
+			var answer = mul;
 			Console.WriteLine($"{answer}");
 		}
 
 		public static void D()
 		{
-			var nk = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nk[0];
-			var k = nk[1];
-			var tlist = new List<int>();
+			var nm = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+			var p = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var abcList = new List<(long a, long b, long c)>();
 
-			for (var i = 0; i < n; ++i) {
-				tlist.Add(int.Parse(Console.ReadLine()));
+			for (var i = 0; i < n - 1; ++i) {
+				var abc = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+				abcList.Add((abc[0], abc[1], abc[2]));
 			}
 
+			var table = new long[n];
+			for (var i = 0; i < p.Length - 1; ++i) {
+				// いもす法（ただし、入場・出場地点に注意
+				var a = p[i] < p[i + 1] ? p[i] : p[i + 1];
+				var b = p[i] < p[i + 1] ? p[i + 1] : p[i];
 
-			var answer = n;
-			if (n - k > 0) {
-				var diffList = new List<(int diff, int index)>();
-				for (var i = 0; i < n - 1; ++i) {
-					diffList.Add((tlist[i + 1] - tlist[i], i));
-				}
-
-				diffList.Sort((a, b) => a.diff.CompareTo(b.diff));
-
-				for (var i = 0; i < n - k; ++i) {
-					answer += diffList[i].diff - 1;
-				}
+				++table[a - 1];
+				--table[b - 1];
 			}
 
+			for (var i = 1; i < n; ++i) {
+				table[i] += table[i - 1];
+			}
 
-			Console.WriteLine($"{answer}");
+			long cost = 0;
+			for (var i = 0; i < n - 1; ++i) {
+				long paper = abcList[i].a * table[i];
+				long card = abcList[i].c + abcList[i].b * table[i];
+				cost += Math.Min(paper, card);
+			}
+
+			Console.WriteLine($"{cost}");
 		}
 
 		public static void E()
