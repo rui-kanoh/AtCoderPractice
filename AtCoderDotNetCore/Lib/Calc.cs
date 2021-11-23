15f3c560c0035debb1c0e4e1d2c60b05dd994c8a
elionix-kanoh <272-kanoh@elionix.com>
2021-11-23 09:57:15 +0900

    カンニング

--- AtCoderDotNetCore/Lib/Calc.cs
+++ AtCoderDotNetCore/Lib/Calc.cs
@@ -509,7 +509,7 @@ namespace AtCoderDotNetCore
 			return ret;
 		}
 
-		// 約数かどうか
+		// 約数の列挙
 		public static long[] GetDivisors(long k, bool doesSort = false)
 		{
 			var list = new List<long>();
@@ -566,6 +566,28 @@ namespace AtCoderDotNetCore
 				}
 			}
 			return res;
+		}
+
+		// 組み合わせ列挙
+		//https://qiita.com/gushwell/items/74a96f56ccb64db3660c
+		public static IEnumerable<T[]> Enumerate<T>(IEnumerable<T> items, int k, bool withRepetition)
+		{
+			if (k == 1) {
+				foreach (var item in items)
+					yield return new T[] { item };
+				yield break;
+			}
+			foreach (var item in items) {
+				var leftside = new T[] { item };
+
+				// item よりも前のものを除く （順列と組み合わせの違い)
+				// 重複を許さないので、unusedから item そのものも取り除く
+				var unused = withRepetition ? items : items.SkipWhile(e => !e.Equals(item)).Skip(1).ToList();
+
+				foreach (var rightside in Enumerate(unused, k - 1, withRepetition)) {
+					yield return leftside.Concat(rightside).ToArray();
+				}
+			}
 		}
 	}
 }
--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -56,14 +56,75 @@ namespace AtCoderDotNetCore
 
 		}
 
+		public static long Gcd(long a, long b)
+		{
+			if (b == 0) {
+				return a;
+			}
+
+			return Gcd(b, a % b);
+		}
+
 		public static void B()
 		{
+			/*
+			 * 余計なことをすると
+			 * 実行時間	2762 ms
+			 * メモリ	24104 KB
+			 * 
+			 * 愚直にやると
+			 * 実行時間	1032 ms
+			 * メモリ	17800 KB
+			 */
 
+			int k = int.Parse(Console.ReadLine());
+
+			long sum = 0;
+			for (var a = 1; a <= k; ++a) {
+				for (var b = 1; b <= k; ++b) {
+					for (var c = 1; c <= k; ++c) {
+						var list = new List<long> { a, b, c };
+						long value = Gcd(a, b);
+						value = Gcd(value, c);
+						sum += value;
+					}
+				}
+			}
+
+			Console.WriteLine($"{sum}");
 		}
 
 		public static void C()
 		{
+			var nm = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+			string name = Console.ReadLine();
+			var dict = new Dictionary<char, int>();
+			foreach (var c in name.ToCharArray()) {
+				if (dict.ContainsKey(c) == false) {
+					dict.Add(c, 1);
+				} else {
+					++dict[c];
+				}
+			}
 
+			string kit = Console.ReadLine();
+
+			int max = 0;
+			foreach (var item in dict) {
+				int charCount = kit.Count(s => s == item.Key);
+				if (charCount == 0) {
+					Console.WriteLine("-1");
+					return;
+				}
+
+				int count = (int)Math.Ceiling((double)item.Value / charCount);
+
+				max = Math.Max(max, count);
+			}
+
+			Console.WriteLine($"{max}");
 		}
 
 		public static void D()
@@ -74,6 +135,22 @@ namespace AtCoderDotNetCore
 		public static void E()
 		{
 
+		}
+
+		public static void Christmas()
+		{
+			var nx = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nx[0];
+			var x = nx[1];
+
+			var burger = "P";
+			for (var i = 1; i <= n; ++i) {
+				burger = $"B{burger}P{burger}B";
+			}
+
+			var sub = burger.Substring(0, (int)x);
+			var answer = sub.Count(s => s == 'P');
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
