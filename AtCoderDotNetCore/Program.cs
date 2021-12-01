b327bee3c3cf7db69165d0afcba941c4bccee554
elionix-kanoh <272-kanoh@elionix.com>
2021-12-01 13:57:11 +0900

    cleanup

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -24,6 +24,19 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			string s = Console.ReadLine();
+
+			long ln = long.Parse(Console.ReadLine());
+			int n = int.Parse(Console.ReadLine());
+
+			string[] inputStrArray = Console.ReadLine().Split(" ");
+
+			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+
+			var answer = 0;
+
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -51,154 +64,25 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			int n = int.Parse(Console.ReadLine());
-			var answer = n % 3 == 0 ? "YES" : "NO";
-			Console.WriteLine($"{answer}");
-		}
-
-		public static long Gcd(long a, long b)
-		{
-			if (b == 0) {
-				return a;
-			}
-
-			return Gcd(b, a % b);
 		}
 
 		public static void B()
 		{
-			/*
-			 * 余計なことをすると
-			 * 実行時間	2762 ms
-			 * メモリ	24104 KB
-			 * 
-			 * 愚直にやると
-			 * 実行時間	1032 ms
-			 * メモリ	17800 KB
-			 */
-
-			int k = int.Parse(Console.ReadLine());
-
-			long sum = 0;
-			for (var a = 1; a <= k; ++a) {
-				for (var b = 1; b <= k; ++b) {
-					for (var c = 1; c <= k; ++c) {
-						long value = Gcd(a, b);
-						value = Gcd(value, c);
-						sum += value;
-					}
-				}
-			}
-
-			Console.WriteLine($"{sum}");
 		}
 
 		public static void C()
 		{
-			var nm = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
-			string name = Console.ReadLine();
-			var dict = new Dictionary<char, int>();
-			foreach (var c in name.ToCharArray()) {
-				if (dict.ContainsKey(c) == false) {
-					dict.Add(c, 1);
-				} else {
-					++dict[c];
-				}
-			}
 
-			string kit = Console.ReadLine();
-
-			int max = 0;
-			foreach (var item in dict) {
-				int charCount = kit.Count(s => s == item.Key);
-				if (charCount == 0) {
-					Console.WriteLine("-1");
-					return;
-				}
-
-				int count = (int)Math.Ceiling((double)item.Value / charCount);
-
-				max = Math.Max(max, count);
-			}
-
-			Console.WriteLine($"{max}");
 		}
 
 		public static void D()
 		{
-			var hw = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var h = hw[0];
-			var w = hw[1];
-			var mas = new char[h, w];
-
-
-			var ruiO = new int[h, w];
-			for (var i = 0; i < h; ++i) {
-				string s = Console.ReadLine();
-				for (var j = 0; j < s.Length; ++j) {
-					mas[i, j] = s[j];
-					if (j == 0) {
-						ruiO[i, j] = 0;
-					} else {
-						ruiO[i, j] = ruiO[i, j - 1];
-						if (s[j] == 'O') {
-							++ruiO[i, j];
-						}
-					}
-				}
-			}
-
-			var ruiI = new int[h, w];
-			for (var j = 0; j < w; ++j) {
-				for (var i = 0; i < h; ++i) {
-					if (i == 0) {
-						ruiI[i, j] = 0;
-					} else {
-						ruiI[i, j] = ruiI[i - 1, j];
-						if (mas[i, j] == 'I') {
-							++ruiI[i, j];
-						}
-					}
-				}
-			}
-
-			long count = 0;
-
-			for (var i = 0; i < h; ++i) {
-				for (var j = 0; j < w; ++j) {
-					if (mas[i, j] == 'J') {
-						long countI = ruiI[h - 1, j] - ruiI[i, j];
-						long countO = ruiO[i, w - 1] - ruiO[i, j];
-						count += countI * countO;
-					}
-				}
-			}
-
-			Console.WriteLine($"{count}");
 
 		}
 
 		public static void E()
 		{
 
-		}
-
-		public static void Christmas()
-		{
-			var nx = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var n = nx[0];
-			var x = nx[1];
-
-			var burger = "P";
-			for (var i = 1; i <= n; ++i) {
-				burger = $"B{burger}P{burger}B";
-			}
-
-			var sub = burger.Substring(0, (int)x);
-			var answer = sub.Count(s => s == 'P');
-			Console.WriteLine($"{answer}");
 		}
 	}
 }
