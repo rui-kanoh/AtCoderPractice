726f98b53b127dc945f856ec1edba4ddc5765b65
elionix-kanoh <272-kanoh@elionix.com>
2021-12-08 08:59:34 +0900

    cleanup

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -52,121 +52,22 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
-			int n = int.Parse(Console.ReadLine());
-			var a = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var b = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
 
-			long max = a[0] * b[0];
-			long maxA = a[0];
-			for (var i = 0; i < n; ++i) {
-				long answer = 0;
-				if (i >= 1) {
-					maxA = Math.Max(maxA, a[i]);
-					long value = maxA * b[i];
-					max = Math.Max(max, value);
-				}
-
-				answer = max;
-
-				Console.WriteLine($"{answer}");
-			}
 		}
 
 		public static void B()
 		{
-			var nq = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nq[0];
-			var q = nq[1];
-			var llist = new List<long>();
-			var rlist = new List<long>();
-			var tlist = new List<long>();
 
-			for (var i = 0; i < q; ++i) {
-				var lrt = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-				llist.Add(lrt[0]);
-				rlist.Add(lrt[1]);
-				tlist.Add(lrt[2]);
-			}
-
-			var a = new long[n];
-			for (var i = 0; i < q; ++i) {
-				for (var j = llist[i] - 1; j <= rlist[i] - 1; ++j) {
-					a[j] = tlist[i];
-				}
-			}
-
-			var builder = new StringBuilder();
-			for (var i = 0; i < n; ++i) {
-				builder.AppendLine($"{a[i]}");
-			}
-
-			Console.WriteLine(builder.ToString());
 		}
 
 		public static void C()
 		{
-			long n = long.Parse(Console.ReadLine());
 
-			var transporters = new long[5];
-			transporters[0] = long.Parse(Console.ReadLine());
-			transporters[1] = long.Parse(Console.ReadLine());
-			transporters[2] = long.Parse(Console.ReadLine());
-			transporters[3] = long.Parse(Console.ReadLine());
-			transporters[4] = long.Parse(Console.ReadLine());
-
-			long min = transporters.Min();
-			long p = (long)Math.Ceiling((double)n / min);
-			long time = (p - 1) + 5;
-
-			Console.WriteLine($"{time}");
 		}
 
 		public static void D()
 		{
-			var nm = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
 
-			var times = new List<long>();
-			var rui = new long[n];
-			for (var i = 0; i < n - 1; ++i) {
-				times.Add(long.Parse(Console.ReadLine()));
-			}
-
-			for (var i = 0; i < n; ++i) {
-				if (i == 0) {
-					rui[0] = 0;
-				} else {
-					rui[i] = rui[i - 1] + times[i - 1];
-				}
-			}
-
-			var dates = new List<(long start, long goal)>();
-			long date = 0;
-			long olddate = 0;
-			for (var i = 0; i < m; ++i) {
-				date = long.Parse(Console.ReadLine());
-				if (date > 0) {
-					dates.Add((olddate, date + olddate));
-				} else {
-					dates.Add((date + olddate, olddate));
-				}
-
-				olddate += date;
-			}
-
-			long total = 0;
-			//var values = new List<long>();
-			foreach (var item in dates) {
-				var value = rui[item.goal] - rui[item.start];
-				//values.Add(value);
-				total += value;
-			}
-
-			long denominator = 10 * 10 * 10 * 10 * 10;
-			var answer = total % denominator;
-
-			Console.WriteLine($"{answer}");
 		}
 
 		public static void E()
