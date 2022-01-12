6fac610aeb9b45070787ad69a80cd54ce42bd6d2
elionix-kanoh <272-kanoh@elionix.com>
2022-01-12 13:46:30 +0900

    3問目をカンニング

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -24,7 +24,19 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
-			
+			long n = long.Parse(Console.ReadLine());
+			long baseNumber = 26;
+
+			string str = "";
+			while (n > 0) {
+				--n;
+				var ans = n % baseNumber;
+				char c = (char)(ans + 'a');
+				str = c.ToString() + str;
+				n /= baseNumber;
+			}
+
+			Console.WriteLine(str);
 		}
 	}
 }
@@ -33,6 +45,27 @@ namespace AtCoderDotNetCore
 {
 	public class Template
 	{
+		// 約数の列挙
+		public static long[] GetDivisors(long k, bool doesSort = false)
+		{
+			var list = new List<long>();
+			long max = (long)Math.Sqrt(k);
+			for (var i = 1; i <= max; ++i) {
+				if (k % i == 0) {
+					list.Add(i);
+					if (i != k / i) {
+						list.Add(k / i);
+					}
+				}
+			}
+
+			if (doesSort) {
+				list.Sort();
+			}
+
+			return list.ToArray();
+		}
+
 		public static void Exec()
 		{
 			string s = Console.ReadLine();
@@ -52,10 +85,46 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			int n = int.Parse(Console.ReadLine());
+			var m = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var point = 80;
+			var answer = 0;
+			for (var i = 0; i < n; ++i) {
+				answer += Math.Max(0, point - m[i]);
+			}
+
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
+			var nab = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nab[0];
+			var a = nab[1];
+			var b = nab[2];
+			int position = 0;
+			for (var i = 0; i < n; ++i) {
+				var sd = Console.ReadLine().Split(" ");
+
+				int dir = sd[0] == "East" ? 1 : -1;
+				int distance = int.Parse(sd[1]);
+				if (distance < a) {
+					distance = a;
+				} else if (b < distance) {
+					distance = b;
+				}
+
+				position += dir * distance;
+
+			}
+
+			if (position == 0) {
+				Console.WriteLine($"0");
+			} else if (position > 0) {
+				Console.WriteLine($"East {position}");
+			} else {
+				Console.WriteLine($"West {Math.Abs(position)}");
+			}
 		}
 
 		public static void C()
@@ -70,7 +139,24 @@ namespace AtCoderDotNetCore
 
 		public static void E()
 		{
+			int q = int.Parse(Console.ReadLine());
+			var answers = new StringBuilder();
+			for (var i = 0; i < q; ++i) {
+				long n = long.Parse(Console.ReadLine());
+				int max = 1;
+				int index = 1000;
+				for (var j = 1; j <= n; ++j) {
+					var divisors = GetDivisors(j);
+					if (max < divisors.Length) {
+						max = divisors.Length;
+						index = j;
+					}
+				}
 
+				answers.AppendLine($"{max} {index}");
+			}
+
+			Console.Write(answers.ToString());
 		}
 
 		public static void F()
