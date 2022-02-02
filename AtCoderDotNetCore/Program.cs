7cd12b4f88ec07a22b5a056c6cb6109443dfa804
elionix-kanoh <272-kanoh@elionix.com>
2022-02-02 09:46:46 +0900

    1問目わからん 3問目WrongAnswer

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -24,17 +24,60 @@ namespace AtCoderDotNetCore
 			Question.Exec();
 		}
 	}
+
 	public static class Question
 	{
-		public static bool IsOdd(long n)
+		// https://c-taquna.hatenablog.com/entry/2020/01/15/014154
+		public static int GridBFS(int h, int w, char[,] map)
 		{
-			bool isOdd = (n & 0x1) == 0x1;
-			return isOdd;
+			int[] vx = { 0, 1, 0, -1 };
+			int[] vy = { 1, 0, -1, 0 };
+
+			var dist = new bool[h, w];
+			var tq = new Queue<(int, int, int)>();
+			int step = 0;
+			tq.Enqueue((0, 0, step));
+			dist[0, 0] = true;
+			while (0 < tq.Count)
+			{
+				var q = tq.Dequeue();
+				int x = q.Item1;
+				int y = q.Item2;
+				step = q.Item3;
+				
+				for (int i = 0; i < 4; i++)
+				{
+					int nx = x + vx[i];
+					int ny = y + vy[i];
+					if ((0 <= nx && nx < h) && (0 <= ny && ny < w) && map[nx, ny] == '.' && dist[nx, ny] == false)
+					{
+						dist[nx, ny] = true;
+						tq.Enqueue((nx, ny, step + 1));
+					}
+				}
+			}
+
+			return step;
 		}
 
 		public static void Exec()
 		{
+			var hw = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var h = hw[0];
+			var w = hw[1];
 
+			var grid = new char[h, w];
+			for (var i = 0; i < h; i++)
+			{
+				var ss = Console.ReadLine();
+				for (var j = 0; j < ss.Length; ++j)
+				{
+					grid[i, j] = ss[j];
+				}
+			}
+
+			int step = GridBFS(h, w, grid);
+			Console.WriteLine($"{step + 1}");
 		}
 	}
 }
@@ -68,12 +111,39 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			var array = Console.ReadLine().Split(" ").Select(i => BigInteger.Parse(i)).ToArray();
+			var a1 = array[0];
+			var a2 = array[1];
+			var a3 = array[2];
 
+			int count = 0;
+			var diff12 = a1 - a2;
+			var diff23 = a2 - a3;
+			if (diff12 == diff23)
+			{
+			}
+			else
+			{
+
+			}
+
+			var answer = count;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
+			int n = int.Parse(Console.ReadLine());
+			var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
 
+			long count = a[0];
+			for (var i = 1; i < n; ++i)
+			{
+				count = count + a[i] + count;
+			}
+
+			long answer = count;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void C()
