1a794da83a0ad2aa70afe17c8c2c872379ea1b45
elionix-kanoh <272-kanoh@elionix.com>
2022-02-09 11:40:06 +0900

    2問目まで

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -29,6 +29,108 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			var n = long.Parse(Console.ReadLine());
+			var switches = Console.ReadLine().Split(" ").Select(i => int.Parse(i) == 1).ToArray();
+
+			long max = 0;
+			long end = 0;
+
+			long sum = 1;
+
+			for (long start = 0; start < n; ++start)
+			{
+				// 交互列が終わるまでendを伸ばす
+				while (end < n)
+				{
+					++end;
+
+					if (end >= n)
+					{
+						end = n - 1;
+						break;
+					}
+
+					if (end > 0)
+					{
+						if (switches[end - 1] != switches[end])
+						{
+							++sum;
+						}
+						else
+						{
+							--end;
+							break;
+						}
+					}
+					else
+					{
+						++sum;
+					}
+				}
+
+				var pluseCount = 0;
+				if (start > 0)
+				{
+					++sum;
+					++pluseCount;
+					if (start > 2 && switches[start - 1] != switches[start - 2])
+					{
+						++sum;
+						++pluseCount;
+					}
+				}
+
+				if (end < switches.Length - 1)
+				{
+					++sum;
+					++pluseCount;
+					if (end < switches.Length - 3 && switches[end + 1] != switches[end + 2])
+					{
+						++sum;
+						++pluseCount;
+					}
+				}
+
+				Console.WriteLine($"{start} {end} {sum} {max}");
+				max = Math.Max(sum, max);
+
+				if (end == start)
+				{
+					++end;
+				}
+
+				sum -= pluseCount;
+
+				if (start < switches.Length - 1
+					&& switches[start] != switches[start + 1]
+					&& sum > 1)
+				{
+					--sum;
+				}
+			}
+
+			var answer = max;
+			/*
+			if (indexes[0] > 0)
+			{
+				++answer;
+				if (indexes[0] > 2 && switches[indexes[0] - 1] != switches[indexes[0] - 2])
+				{
+					++answer;
+				}
+			}
+
+			if (indexes[1] < switches.Length - 1)
+			{
+				++answer;
+				if (indexes[1] < switches.Length - 3 && switches[indexes[1] + 1] != switches[indexes[1] + 2])
+				{
+					++answer;
+				}
+			}
+			*/
+
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -62,22 +164,72 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			string s = Console.ReadLine();
 
+			var answer = s.Count(s => s == '1');
+
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
 		{
+			var nxy = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nxy[0];
+			var x = nxy[1];
+			var y = nxy[2];
 
+			var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToList();
+			a.Sort();
+
+			bool isDrawed = false;
+			bool winsT = false;
+			var tlist = new List<int>();
+			tlist.Add(x);
+			var aokiList = new List<int>();
+			aokiList.Add(y);
+
+			for (var i = 0; i < n / 2; ++i)
+			{
+				tlist.Add(a.Last());
+				a.RemoveAt(a.Count - 1);
+				aokiList.Add(a.Last());
+				a.RemoveAt(a.Count - 1);
+			}
+
+			var sumT = tlist.Sum();
+			var sumA = aokiList.Sum();
+			if (sumT == sumA)
+			{
+				isDrawed = true;
+			}
+			else
+			{
+				winsT = sumT > sumA;
+			}
+
+			var answer = isDrawed
+				? "Draw"
+				: winsT ? "Takahashi" : "Aoki";
+			Console.WriteLine($"{answer}");
 		}
 
 
 		public static void C()
 		{
-	
+			var nm = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+			var alist = new List<int>();
+			var blist = new List<int>();
+			for (var i = 0; i < n; i++)
+			{
+				var ab = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			}
 		}
 
 		public static void D()
 		{
+
 		}
 
 		public static void E()
