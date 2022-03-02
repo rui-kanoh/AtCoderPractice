b0ae0a75b312f8e31880296e06fa79be58092468
elionix-kanoh <272-kanoh@elionix.com>
2022-03-02 09:43:16 +0900

    少しマシになった

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -27,21 +27,60 @@ namespace AtCoderDotNetCore
 
 	public static class Question
 	{
+		public static int GridBFS(int h, int w, int sx, int sy, int gx, int gy)
+		{
+			int[] vx = { 0, 1, 0, -1 };
+			int[] vy = { 1, 0, -1, 0 };
+
+			if (sx == gx && sy == gy)
+			{
+				return 0;
+			}
+
+			var width = Math.Abs(gx - sx) + 1;
+			var height = Math.Abs(gy - sy) + 1;
+
+			var dist = new bool[width, height];
+			var tq = new Queue<(int, int, int)>();
+			int step = 0;
+			tq.Enqueue((sx, sy, step));
+			dist[0, 0] = true;
+			while (0 < tq.Count)
+			{
+				var q = tq.Dequeue();
+				int x = q.Item1;
+				int y = q.Item2;
+				step = q.Item3;
+
+				if (x == gx && y == gy)
+				{
+					break;
+				}
+
+				for (int i = 0; i < 4; i++)
+				{
+					int nx = x + vx[i] - sx;
+					int ny = y + vy[i] - sy;
+					if ((0 <= nx && nx < width) && (0 <= ny && ny < height) && dist[nx, ny] == false)
+					{
+						dist[nx, ny] = true;
+						tq.Enqueue((nx + sx, ny + sy, step + 1));
+					}
+				}
+			}
+
+			return step;
+		}
+
 		public static void Exec()
 		{
-			string s = Console.ReadLine();
+			var x1y1 = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var x2y2 = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
 
-			long ln = long.Parse(Console.ReadLine());
-			int n = int.Parse(Console.ReadLine());
-
-			string[] inputStrArray = Console.ReadLine().Split(" ");
-
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-
-			var answer = 0;
-
-			Console.WriteLine($"{answer}");	
+			int size = 100000;
+			var step = GridBFS(size, size, x1y1[0] - 1, x1y1[1] - 1, x2y2[0] - 1, x2y2[1] - 1);
+			var answer = step + 1;
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -75,6 +114,9 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			string s = Console.ReadLine();
+			var answer = $"ABC{s}";
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void B()
