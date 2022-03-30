1e88a66a68f0f4b1f53d85373521f6c189102595
elionix-kanoh <272-kanoh@elionix.com>
2022-03-30 11:02:41 +0900

    青木まで

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -29,7 +29,19 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
-			var answer = count;
+			long a = long.Parse(Console.ReadLine());
+			long b = long.Parse(Console.ReadLine());
+			long n = long.Parse(Console.ReadLine());
+
+			var answer = a * b;
+			for (var i = n; i <= 30000; ++i)
+			{
+				if (((i % a) == 0) && ((i % b) == 0))
+				{
+					answer = i;
+					break;
+				}
+			}
 
 			Console.WriteLine($"{answer}");
 		}
@@ -65,116 +77,269 @@ namespace AtCoderDotNetCore
 
 		public static void A()
 		{
+			string s = Console.ReadLine();
+			var answer = s.Last() == 'T';
 
+			Console.WriteLine(answer ? "YES" : "NO");
 		}
 
 		public static void B()
 		{
+			string s = Console.ReadLine();
+			int count = 0;
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (s[i] == '+')
+				{
+					++count;
+				}
+				else
+				{
+					--count;
+				}
+			}
+
+			Console.WriteLine($"{count}");
+		}
+
+		public static bool IsPrime(long num)
+		{
+			if (num < 2) return false;
+			else if (num == 2) return true;
+			else if (num % 2 == 0) return false;
+
+			double sqrtNum = Math.Sqrt(num);
+			for (int i = 3; i <= sqrtNum; i += 2)
+			{
+				if (num % i == 0)
+				{
+					// 素数ではない
+					return false;
+				}
+			}
+
+			// 素数である
+			return true;
 		}
 
 		public static void C()
 		{
-			string s = Console.ReadLine();
-			var oklist = new List<int>();
-			var qlist = new List<int>();
-			var nglist = new List<int>();
-			for (var i = 0; i < s.Length; i++)
-			{
-				if (s[i] == 'o')
-				{
-					oklist.Add(i);
-				}
-				else if (s[i] == '?')
-				{
-					qlist.Add(i);
-				}
-				else
-				{
-					nglist.Add(i);
-				}
-			}
-
-			if (oklist.Count > 4)
-			{
-				Console.WriteLine("0");
-				return;
-			}
-
 			int count = 0;
-			void Dfs(List<int> items, int num)
+			int n = int.Parse(Console.ReadLine());
+			for (var i = 1; i < n; i++)
 			{
-				if (items.Count == num)
+				if (IsPrime(i))
 				{
-					/*
-					foreach (var item in items)
-					{
-						Console.Write($"{item} ");
-					}
-					Console.WriteLine("");
-					*/
+					++count;
+				}
+			}
 
-					bool isOK = true;
-					foreach (var item in items)
-					{
-						if (nglist.Contains(item))
-						{
-							isOK = false;
-							break;
-						}
-					}
+			Console.WriteLine($"{count}");
+		}
 
-					foreach (var okitem in oklist)
-					{
-						if (items.Contains(okitem) == false)
-						{
-							isOK = false;
-							break;
-						}
-					}
+		/*
+		public static void niko()
+        {
+			int count = 0;
+			long n = long.Parse(Console.ReadLine());
 
-					if (isOK)
+
+			var niko = new int[] { 25, 2525, 252525, 25252525, };
+
+			var hash = new HashSet<long>();
+			foreach (var item in niko)
+			{
+				var list = GetDivisors(item);
+				foreach (var l in list)
+				{
+					if (hash.Contains(l) == false)
 					{
+						hash.Add(l);
 						++count;
 					}
-
-					return;
-				}
-
-				for (var i = 0; i < 10; ++i)
-				{
-					items.Add(i);
-					Dfs(items, num);
-					items.RemoveAt(items.Count - 1);
 				}
 			}
-
-			Dfs(new List<int>(), 4);
-
-			var answer = count;
-
-			Console.WriteLine($"{answer}");
 		}
+		*/
 
 		public static void D()
 		{
+			int n = int.Parse(Console.ReadLine());
+			var answer = (9.0 / 5.0 * n) + 32.0;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void E()
 		{
+			string[] ab = Console.ReadLine().Split(" ");
+			var a = ab[0];
+			var b = ab[1];
+
+			var answer = true;
+			if ((a == "H" && b == "H")
+				|| (a == "D" && b == "D"))
+			{
+				answer = true;
+			}
+			else
+			{
+				answer = false;
+			}
+
+			Console.WriteLine(answer ? "H" : "D");
+
+
 		}
 
 		public static void F()
 		{
+			string[] sss = Console.ReadLine().Split(" ");
+			var s1 = sss[0];
+			var s2 = sss[1];
+			var s3 = sss[2];
+
+
+			var answer = (s1[0].ToString() + s2[0].ToString() + s3[0].ToString()).ToUpper();
+
+			Console.WriteLine($"{answer}");
+
 		}
 
 		public static void G()
 		{
+			long n = int.Parse(Console.ReadLine());
+			var list = new List<int>();
+			for (var i = 0; i < n; i++)
+			{
+				list.Add(int.Parse(Console.ReadLine()));
+			}
 
+			list.Sort();
+
+			var answer = list[0];
+
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void H()
 		{
+			var aop = Console.ReadLine().Split(" ");
+			var a = int.Parse(aop[0]);
+			var op = aop[1];
+			var b = int.Parse(aop[2]);
 
+			var answer = 0;
+			if (op == "+")
+			{
+				answer = a + b;
+			}
+			else
+			{
+				answer = a - b;
+			}
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void J()
+		{
+			int n = int.Parse(Console.ReadLine());
+			var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var list = a.Where(s => s > 0).ToList();
+			double ave = list.Average();
+			var answer = (int)Math.Ceiling(ave);
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void aoki()
+		{
+			long a = long.Parse(Console.ReadLine());
+			long b = long.Parse(Console.ReadLine());
+			long n = long.Parse(Console.ReadLine());
+
+			var answer = a * b;
+			for (var i = n; i <= (a * b); ++i)
+			{
+				if (((i % a) == 0) && ((i % b) == 0))
+				{
+					answer = i;
+					break;
+				}
+			}
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void K()
+		{
+			int n = int.Parse(Console.ReadLine());
+			var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToList();
+			a.Sort();
+			a.Reverse();
+			var answer = 0;
+			for (int i = 0; i < n; i = i + 2)
+			{
+				answer += a[i];
+			}
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void KK()
+		{
+			string x = Console.ReadLine();
+			string s = Console.ReadLine();
+			var answer = s.Replace(x, "");
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void KKK()
+        {
+			var abc = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var answer = abc[0] <= abc[2] && abc[2] <= abc[1];
+
+			Console.WriteLine(answer ? "Yes" : "No");
+		}
+
+		public static void KKKK()
+        {
+			var a = BigInteger.Parse(Console.ReadLine());
+			var b = BigInteger.Parse(Console.ReadLine());
+
+			var answer = "EQUAL";
+			if (a > b)
+			{
+				answer = "GREATER";
+			}
+			else if (a < b)
+			{
+				answer = "LESS";
+			}
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void KKKKKK()
+		{
+			var a = int.Parse(Console.ReadLine());
+			var b = int.Parse(Console.ReadLine());
+
+			var answer = 0;
+			if (a % b != 0)
+			{
+				for (var i = 0; i < a; i++)
+				{
+					++a;
+					++answer;
+					if (a % b == 0)
+					{
+						break;
+					}
+				}
+			}
+
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
