a70b65882e58d6b5084c589db9dd64e0b341b981
elionix-kanoh <272-kanoh@elionix.com>
2022-04-04 16:47:39 +0900

    清書

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -27,123 +27,8 @@ namespace AtCoderDotNetCore
 
 	public static class Question
 	{
-		public static (int left, int right) BinarySearch(int value, List<int> list)
-		{
-			int left = -1;
-			int right = list.Count;
-			while (right - left > 1)
-			{
-				int mid = (right + left) / 2;
-				if (list[mid] == value)
-				{
-					return (mid, mid);
-				}
-				else if (list[mid] > value)
-				{
-					right = mid;
-				}
-				else
-				{
-					left = mid;
-				}
-			}
-
-			return (left, right);
-		}
-
-		public static bool IsOdd(long n)
-		{
-			bool isOdd = (n & 0x1) == 0x1;
-			return isOdd;
-		}
-
 		public static void Exec()
 		{
-			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
-			var h = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var hlist = h.ToList();
-			hlist.Sort();
-			var w = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-
-			var h1 = new int[(n + 1) / 2 - 1];
-			var h2 = new int[(n + 1) / 2 - 1];
-			var rui1 = new int[(n + 1) / 2];
-			var rui2 = new int[(n + 1) / 2];
-			for (var i = 1; i < (n + 1) / 2; ++i)
-            {
-				h1[i - 1] = Math.Abs(hlist[2 * (i - 1)] - hlist[2 * (i - 1) + 1]);
-				h2[i - 1] = Math.Abs(hlist[2 * i - 1] - hlist[2 * i]);
-				rui1[i] = rui1[i - 1] + h1[i - 1];
-				rui2[i] = rui2[i - 1] + h2[i - 1];
-			}
-
-			/*
-			var builder = new StringBuilder();
-			for (var i = 0; i < hlist.Count; ++i)
-			{
-				builder.Append(hlist[i] + " ");
-			}
-			Console.WriteLine(builder.ToString());
-			builder.Clear();
-
-			for (var i = 0; i < rui1.Length; ++i)
-			{
-				builder.Append(rui1[i] + " ");
-			}
-			Console.WriteLine(builder.ToString());
-			builder.Clear();
-
-			for (var i = 0; i < rui2.Length; ++i)
-			{
-				builder.Append(rui2[i] + " ");
-			}
-			Console.WriteLine(builder.ToString());
-			builder.Clear();
-			*/
-
-			var min = int.MaxValue;
-			for (var i = 0; i < w.Length; ++i)
-			{
-				(var left, var right) = BinarySearch(w[i], hlist);
-				int count = 0;
-				if (left == -1)
-				{
-					count = rui2[rui2.Length - 1] + Math.Abs(hlist[0] - w[i]);
-				}
-				else if (right == n)
-				{
-					count = rui1[rui1.Length - 1] + Math.Abs(hlist[n - 1] - w[i]);
-				} else
-                {
-					int index = left / 2;
-					
-					int value = 0;
-					int value2 = 0;
-					if (IsOdd(left))
-                    {
-						count = (rui1[index + 1] - rui1[0]);
-						value = Math.Abs(hlist[left + 1] - w[i]);
-						value2 = (rui2[rui2.Length - 1] - rui2[index + 1]);
-					} else
-                    {
-						count = (rui1[index] - rui1[0]);
-						value = Math.Abs(hlist[left] - w[i]);
-						value2 = (rui2[rui2.Length - 1] - rui2[index]);
-					}
-
-					count += value + value2;
-					//Console.WriteLine($"{w[i]}, {left}, {right}, {index}, {value}, {value2}, {count}");
-				}
-
-				//Console.WriteLine($"{w[i]}, {count}");
-				min = Math.Min(min, count);
-			}
-
-			var answer = min;
-
-			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -200,6 +85,96 @@ namespace AtCoderDotNetCore
 			{
 				answer /= 2;
 			}
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static (int left, int right) BinarySearch(int value, List<int> list)
+		{
+			int left = -1;
+			int right = list.Count;
+			while (right - left > 1)
+			{
+				int mid = (right + left) / 2;
+				if (list[mid] == value)
+				{
+					return (mid, mid);
+				}
+				else if (list[mid] > value)
+				{
+					right = mid;
+				}
+				else
+				{
+					left = mid;
+				}
+			}
+
+			return (left, right);
+		}
+
+		public static void TransfomableTeacher()
+        {
+
+			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+			var h = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var hlist = h.ToList();
+			hlist.Sort();
+			var w = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+
+			var h1 = new int[(n + 1) / 2 - 1];
+			var h2 = new int[(n + 1) / 2 - 1];
+			var rui1 = new int[(n + 1) / 2];
+			var rui2 = new int[(n + 1) / 2];
+			for (var i = 1; i < (n + 1) / 2; ++i)
+			{
+				h1[i - 1] = Math.Abs(hlist[2 * (i - 1)] - hlist[2 * (i - 1) + 1]);
+				h2[i - 1] = Math.Abs(hlist[2 * i - 1] - hlist[2 * i]);
+				rui1[i] = rui1[i - 1] + h1[i - 1];
+				rui2[i] = rui2[i - 1] + h2[i - 1];
+			}
+
+			var min = int.MaxValue;
+			for (var i = 0; i < w.Length; ++i)
+			{
+				(var left, var right) = BinarySearch(w[i], hlist);
+				int count = 0;
+				if (left == -1)
+				{
+					count = rui2[rui2.Length - 1] + Math.Abs(hlist[0] - w[i]);
+				}
+				else if (right == n)
+				{
+					count = rui1[rui1.Length - 1] + Math.Abs(hlist[n - 1] - w[i]);
+				}
+				else
+				{
+					int index = left / 2;
+
+					int value = 0;
+					int value2 = 0;
+					if (IsOdd(left))
+					{
+						count = (rui1[index + 1] - rui1[0]);
+						value = Math.Abs(hlist[left + 1] - w[i]);
+						value2 = (rui2[rui2.Length - 1] - rui2[index + 1]);
+					}
+					else
+					{
+						count = (rui1[index] - rui1[0]);
+						value = Math.Abs(hlist[left] - w[i]);
+						value2 = (rui2[rui2.Length - 1] - rui2[index]);
+					}
+
+					count += value + value2;
+				}
+
+				min = Math.Min(min, count);
+			}
+
+			var answer = min;
 
 			Console.WriteLine($"{answer}");
 		}
