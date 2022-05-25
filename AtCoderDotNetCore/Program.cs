b963e864a182865240fb327cc71aabb91625242f
elionix-kanoh <272-kanoh@elionix.com>
2022-05-25 16:43:28 +0900

    2問目まで

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -29,18 +29,14 @@ namespace AtCoderDotNetCore
 	{
 		public static void ExecTemp()
 		{
+			var stamps = new[] { 'J', 'O', 'I' };
+			long n = long.Parse(Console.ReadLine());
 			string s = Console.ReadLine();
 
-			long ln = long.Parse(Console.ReadLine());
-			int n = int.Parse(Console.ReadLine());
-
-			string[] inputStrArray = Console.ReadLine().Split(" ");
-
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			// DPなんだろうけれど。。
+			long max = 0;
 
 			var answer = 0;
-
 			Console.WriteLine($"{answer}");
 		}
 
@@ -77,6 +73,85 @@ namespace AtCoderDotNetCore
 			bool isOdd = (n & 0x1) == 0x1;
 			return isOdd;
 		}
+
+		public static void Acrostic()
+		{
+			string S = Console.ReadLine();
+			int w = int.Parse(Console.ReadLine());
+			int num = (S.Length / w) + (S.Length % w > 0 ? 1 : 0);
+
+			var strs = new string[num];
+			for (int i = 0; i < num; i++)
+			{
+				int length = (w * i + w) >= S.Length ? S.Length - (w * i) : w;
+				strs[i] = S.Substring(w * i, length);
+			}
+
+			string str = "";
+			foreach (var item in strs)
+			{
+				str += item[0].ToString();
+			}
+
+			var answer = str;
+
+			Console.WriteLine($"{answer}");
+		}
+
+
+		public static void Niko()
+		{
+			long n = long.Parse(Console.ReadLine());
+
+			long count = 0;
+			for (var i = 1; i <= n; ++i)
+			{
+				if (i % 25 == 0)
+				{
+					++count;
+				}
+			}
+
+			var answer = count;
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void StreamLine()
+		{
+			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+			var x = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToList();
+			x.Sort();
+
+			int count = 0;
+			if (n >= m)
+			{
+				count = 0;
+			}
+			else
+			{
+				// Sortして、外れ値を n - 1個除去してから、残りのグループの最小と最大の差が答え？
+				// ただ、グループが複数個の分散していた場合は対応できない。
+				long remain = m - n;
+
+				// n個のグループをつくる
+				// グループ分けのアルゴリズムが全く分からない
+				var groups = new List<int>[n];
+				var lengthArray = new long[m];
+				for (var i = 0; i < m - 1; ++i)
+				{
+					lengthArray[i] = x[i + 1] - x[i];
+				}
+			}
+
+
+			var answer = count;
+
+			Console.WriteLine($"{answer}");
+		}
+
 	}
 }
 
