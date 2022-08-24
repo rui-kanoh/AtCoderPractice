595a6bfc5074fd9113538d04456156fc4e1e25a0
elionix-kanoh <272-kanoh@elionix.com>
2022-08-24 10:08:45 +0900

    4問目までカンニングしないで解けた。5問目入力まで。

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -28,6 +28,49 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
+			ExecTemp();
+		}
+
+		public static void ExecTemp()
+		{
+			var mn = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var m = mn[0];
+			var n = mn[1];
+			var abcList = new List<(int a, int b, int c)>();
+			// 連接リスト
+			var lists = new List<int>[m];
+
+			for (var i = 0; i < n; ++i) {
+				lists[i] = new List<int>();
+			}
+
+
+			for (var i = 0; i < m; ++i) {
+				var abc = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				var a = abc[0] - 1;
+				var b = abc[1] - 1;
+				var c = abc[2];
+				abcList.Add((a, b, c));
+
+				lists[a].Add(b);
+				lists[b].Add(a);
+			}
+
+			int q = int.Parse(Console.ReadLine());
+			var stList = new List<(int s, int t)>();
+			for (var i = 0; i < n; ++i) {
+				var st = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				var s = st[0];
+				var t = st[1];
+				stList.Add((s, t));
+			}
+
+			var answer = 0;
+			if (q == 1) {
+				Console.WriteLine($"{answer}");
+			} else {
+				Console.WriteLine($"{answer}");
+			}
 		}
 	}
 }
@@ -59,6 +102,139 @@ namespace AtCoderDotNetCore
 			return isOdd;
 		}
 
+		public static void Exec1222()
+		{
+			string s = Console.ReadLine();
+			var answer = s.Count(c => c == '2');
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void Walkman()
+		{
+			var nk = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nk[0];
+			var k = nk[1];
+			var a = new long[n];
+			for (var i = 0; i < n; ++i) {
+				a[i] = long.Parse(Console.ReadLine());
+			}
+
+			long count = 0;
+			var answer = 0;
+			for (var i = 0; i < n; ++i) {
+				count += a[i];
+				if (count >= k) {
+					answer = i + 1;
+					break;
+				}
+			}
+
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void MazeMaster()
+		{
+			var hw = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var h = hw[0];
+			var w = hw[1];
+
+			var map = new bool[h, w];
+			for (var i = 0; i < h; i++) {
+				string s = Console.ReadLine();
+				for (var j = 0; j < s.Length; ++j) {
+					map[i, j] = s[j] == '.';
+				}
+			}
+
+			var map2 = new bool[h, w];
+			void Initialize()
+			{
+				for (var i = 0; i < h; i++) {
+					for (var j = 0; j < w; ++j) {
+						map2[i, j] = map[i, j];
+					}
+				}
+			}
+
+			long max = 0;
+
+			// s/gを任意に決めるための4重ループ sとgを同じ点にできないこと注意
+			for (var i = 0; i < h; i++) {
+				for (var j = 0; j < w; ++j) {
+					for (var k = 0; k < h; k++) {
+						for (var l = 0; l < w; ++l) {
+							if (i == k && j == l) {
+								continue;
+							}
+
+							Initialize();
+
+							int step = Calc.GridBFS(h, w, i, j, k, l, map2);
+							max = Math.Max(step, max);
+						}
+					}
+				}
+			}
+
+			var answer = max;
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void CardSort()
+		{
+			int n = int.Parse(Console.ReadLine());
+			int k = int.Parse(Console.ReadLine());
+			var a = new int[n];
+			for (var i = 0; i < n; ++i) {
+				a[i] = int.Parse(Console.ReadLine());
+			}
+
+			var ans = new HashSet<int>();
+			int count = 0;
+
+			void Dfs(List<int> items, HashSet<int> hash)
+			{
+				if (items.Count == k) {
+					/*
+					for (var i = 0; i < items.Count; ++i) {
+						Console.Write($"{a[items[i]]}");
+					}
+					Console.WriteLine("");
+					*/
+
+					string str = "";
+					for (var i = 0; i < items.Count; ++i) {
+						str += $"{a[items[i]]}";
+					}
+
+					var value = int.Parse(str);
+					if (ans.Contains(value) == false) {
+						ans.Add(value);
+						++count;
+					}
+
+					return;
+				}
+
+				for (int i = 0; i < n; i++) {
+					if (hash.Contains(i) == false) {
+						items.Add(i);
+						hash.Add(i);
+						Dfs(items, hash);
+						hash.Remove(items[items.Count - 1]);
+						items.RemoveAt(items.Count - 1);
+					}
+				}
+			}
+
+			Dfs(new List<int>(), new HashSet<int>());
+
+			var answer = count;
+
+			Console.WriteLine($"{answer}");
+		}
+		
 		public static void TakahashiAoki()
 		{
 			int a = int.Parse(Console.ReadLine());
