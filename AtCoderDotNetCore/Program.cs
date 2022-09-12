3a5457cacd674b4cf42feaff27225f919b048da5
elionix-kanoh <272-kanoh@elionix.com>
2022-09-12 15:26:43 +0900

    カンニングしたのに解けなかったので断念

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -13,6 +13,7 @@ using System.Buffers.Text;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.Intrinsics.X86;
+using System.Reflection;
 
 namespace AtCoderDotNetCore
 {
@@ -37,126 +38,28 @@ namespace AtCoderDotNetCore
 			var n = nq[0];
 			var q = nq[1];
 			string s = Console.ReadLine();
-			var deq = new Deque<char>(s.Length);
-			for (var i = 0; i < s.Length; ++i) {
-				deq.PushBack(s[i]);
-			}
 
 			var builder = new StringBuilder();
 
+			int count = 0;
 			for (var i = 0; i < q; ++i) {
 				var query = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
 				var command = query[0];
 				var x = query[1];
 				if (command == 1) {
-					for (var j = 0; j < x; ++j) {
-						char c = deq.PopBack();
-						deq.PushFront(c);
-					}
+					count += x;
 				} else {
-					builder.AppendLine($"{deq[x - 1]}");
+					int index = (x - 1) - (count % n);
+					if (index < 0) {
+						index = s.Length + index;
+					}
+
+					builder.AppendLine($"{s[index]}");
 				}
 			}
 
-
 			var answer = builder.ToString();
 			Console.WriteLine($"{answer}");
-		}
-	}
-
-	public class Deque<T> : IEnumerable<T>
-	{
-		public T this[int i]
-		{
-			get { return Buffer[(FirstIndex + i) % Capacity]; }
-			set
-			{
-				if (i < 0) throw new ArgumentOutOfRangeException();
-				Buffer[(FirstIndex + i) % Capacity] = value;
-			}
-		}
-
-		private T[] Buffer;
-		private int Capacity;
-		private int FirstIndex;
-		private int LastIndex
-		{
-			get { return (FirstIndex + Length) % Capacity; }
-		}
-
-		public int Length;
-
-		public Deque(int capacity = 16)
-		{
-			Capacity = capacity;
-			Buffer = new T[Capacity];
-			FirstIndex = 0;
-		}
-
-		public void PushBack(T data)
-		{
-			if (Length == Capacity) Resize();
-			Buffer[LastIndex] = data;
-			Length++;
-		}
-
-		public void PushFront(T data)
-		{
-			if (Length == Capacity) {
-				Resize();
-			}
-
-			var index = FirstIndex - 1;
-			if (index < 0) {
-				index = Capacity - 1;
-			}
-
-			Buffer[index] = data;
-			Length++;
-			FirstIndex = index;
-		}
-
-		public T PopBack()
-		{
-			if (Length == 0) throw new InvalidOperationException("データが空です。");
-			var data = this[Length - 1];
-			Length--;
-			return data;
-		}
-
-		public T PopFront()
-		{
-			if (Length == 0) throw new InvalidOperationException("データが空です。");
-			var data = this[0];
-			FirstIndex++;
-			FirstIndex %= Capacity;
-			Length--;
-			return data;
-		}
-
-		private void Resize()
-		{
-			var newArray = new T[Capacity * 2];
-			for (int i = 0; i < Length; i++) {
-				newArray[i] = this[i];
-			}
-			FirstIndex = 0;
-			Capacity *= 2;
-			Buffer = newArray;
-		}
-
-		public IEnumerator<T> GetEnumerator()
-		{
-			for (int i = 0; i < Length; i++) {
-				yield return this[i];
-			}
-		}
-
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			for (int i = 0; i < Length; i++) {
-				yield return this[i];
-			}
 		}
 	}
 }
