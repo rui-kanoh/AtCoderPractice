7ff5034f1b912c7dcab6d6a0f1390821e362417e
elionix-kanoh <272-kanoh@elionix.com>
2022-10-05 08:54:05 +0900

    cleanup

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -76,64 +76,6 @@ namespace AtCoderDotNetCore
 			long g = Gcd(a, b);
 			return a / g * b;
 		}
-
-		public static void Parking()
-		{
-			var nab = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nab[0];
-			var a = nab[1];
-			var b = nab[2];
-
-			var answer = Math.Min(a * n, b);
-			Console.WriteLine($"{answer}");
-		}
-
-		public static void Heiho()
-		{
-			int n = int.Parse(Console.ReadLine());
-			string s = Console.ReadLine();
-
-			if (IsOdd(n)) {
-				Console.WriteLine($"-1");
-				return;
-			}
-
-			int half = s.Length / 2;
-			int count = 0;
-			for (var i = 0; i < half; ++i) {
-				if (s[i] != s[half + i]) {
-					++count;
-				}
-			}
-
-			var answer = count;
-			Console.WriteLine($"{answer}");
-		}
-
-		public static void Friends()
-		{
-			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
-
-			var union = new Lib.UnionFind(n);
-			for (var j = 0; j < m; ++j) {
-				var ab = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-				var a = ab[0] - 1;
-				var b = ab[1] - 1;
-				union.Union(a, b);
-			}
-
-			int count = union.GroupCount;
-
-			int max = 0;
-			for (var i = 0; i < count; ++i) {
-				max = Math.Max(max, union.Count(i));
-			}
-
-			var answer = max;
-			Console.WriteLine($"{answer}");
-		}
 	}
 }
 
