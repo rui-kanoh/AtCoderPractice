43ddba45001166c66f1516868dfb7f679e65cb8b
elionix-kanoh <272-kanoh@elionix.com>
2023-02-15 08:58:17 +0900

    記録

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -15,6 +15,7 @@ using System.Globalization;
 using System.Runtime.Intrinsics.X86;
 using System.Reflection;
 using System.Drawing;
+using System.Net;
 
 namespace AtCoderDotNetCore
 {
@@ -35,59 +36,20 @@ namespace AtCoderDotNetCore
 
 		public static void ExecTemp()
 		{
-			var hwn = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var h = hwn[0];
-			var w = hwn[1];
-			var n = hwn[2];
+			int n = int.Parse(Console.ReadLine());
+			var m = 2 * n;
+			var x = 0;
 
-			var rows = new int[h];
-			var cols = new int[w];
-			var dict = new (int a, int b)[n];
-
-			for (var i = 0; i < n; ++i) {
-				var ab = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-				var a = ab[0] - 1;
-				var b = ab[1] - 1;
-				dict[i] = (a, b);
-
-				rows[a] = 1;
-				cols[b] = 1;
-			}
-
-			var ruiRows = new int[h];
-			for (var i = 0; i < h; ++i) {
-				if (i == 0) {
-					ruiRows[i] = rows[i];
-				} else {
-					ruiRows[i] = rows[i] + ruiRows[i - 1];
-				}
-			}
-
-			var ruiCols = new int[w];
-			for (var i = 0; i < w; ++i) {
-				if (i == 0) {
-					ruiCols[i] = cols[i];
-				} else {
-					ruiCols[i] = cols[i] + ruiCols[i - 1];
-				}
-			}
-
-			var buider = new StringBuilder();
-			for (var i = 0; i < n; ++i) {
-				(var r, var c) = dict[i];
-
-				int indexR = r == ruiRows.Length - 1 ? ruiRows.Length - 1 : r;
-				var numR = ruiRows[indexR];
-
-				int indexC = c == ruiCols.Length - 1 ? ruiCols.Length - 1 : c;
-				var numC = ruiCols[indexC];
-
-				buider.AppendLine($"{numR} {numC}");
+			// mについて [1, m - m/2]の範囲で探索してみれば良さそう
+			// mになる和の組み合わせを全列挙して、それを1/2したときにnになるものを探す
+			long min = long.MaxValue;
+			for (var i = 1; i <= m - m / 2; ++i) {
+				long value = m - i;
 			}
 
 
-			var answer = buider.ToString();
-			Console.Write($"{answer}");
+			var answer = $"{min} {x}";
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
@@ -134,148 +96,93 @@ namespace AtCoderDotNetCore
 			return a / g * b;
 		}
 
-		public static void Same()
+		public static void DoubleStrings()
 		{
 			string s = Console.ReadLine();
-			var answer = (s[0] == s[1] && s[1] == s[2] && s[2] == s[3]) ? "SAME" : "DIFFERENT";
+			var answer = s + s;
 			Console.WriteLine($"{answer}");
 		}
 
-		public static void Syoritsu()
+		public static void TensuHenkan()
 		{
-			var abcd = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var a = abcd[0];
-			var b = abcd[1];
-			var c = abcd[2];
-			var d = abcd[3];
-			var lcm = Lcm(a, c);
+			var nab = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nab[0];
+			var a = nab[1];
+			var b = nab[2];
 
-			long temp = lcm / a;
-			long taka = b * temp;
-			temp = lcm / c;
-			long aoki = d * temp;
-			if (taka == aoki) {
-				Console.WriteLine($"DRAW");
-			} else {
-				if (taka > aoki) {
-					Console.WriteLine($"TAKAHASHI");
-				} else {
-					Console.WriteLine($"AOKI");
-				}
-			}
-		}
-
-		public static void SnakeToy()
-		{
-			var nk = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nk[0];
-			var k = nk[1];
-			var l = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToList();
-			l.Sort((a, b) => b.CompareTo(a));
-
-			int count = 0;
-			for (var i = 0; i < k; ++i) {
-				count += l[i];
+			var s = new List<long>();
+			for (var i = 0; i < n; ++i) {
+				s.Add(long.Parse(Console.ReadLine()));
 			}
 
-			var answer = count;
+			var sum = s.Sum();
+			var max = s.Max();
+			var min = s.Min();
+
+			// Pが無い場合
+			if (max == min) {
+				Console.WriteLine($"-1");
+				return;
+			}
+
+			decimal p = (decimal)b / (max - min);
+			decimal q = (decimal)a - p * sum / n;
+
+			var answer = $"{p} {q}";
 			Console.WriteLine($"{answer}");
 		}
 
-		public static void Celcius()
+		public static void TakahashiInformation()
 		{
-			int n = int.Parse(Console.ReadLine());
+			var c = new int[3, 3];
 
-			var answer = (9.0 / 5.0 * n) + 32.0;
-			Console.WriteLine($"{answer}");
-		}
-
-		public static void KenkenRace()
-		{
-			var nabcd = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nabcd[0];
-			var a = nabcd[1] - 1;
-			var b = nabcd[2] - 1;
-			var c = nabcd[3] - 1;
-			var d = nabcd[4] - 1;
-			string s = Console.ReadLine();
-			var mass = new bool[s.Length];
-
-			bool isOK = true;
-			for (var i = 0; i < s.Length; ++i) {
-				mass[i] = s[i] == '.';
-
-				if ((i == c || i == d) && mass[i] == false) {
-					isOK = false;
-					break;
-				}
-
-				if (i > 0 && i >= a && i <= d) {
-					if (mass[i - 1] == false && mass[i] == false) {
-						isOK = false;
-						break;
-					}
+			for (var i = 0; i < 3; ++i) {
+				var cc = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				for (var j = 0; j < 3; ++j) {
+					c[i, j] = cc[j];
 				}
 			}
 
-			if (isOK) {
-				if (c < d) {
-					isOK = true;
-				} else {
-					bool exists3blank = false;
-					for (var i = b; i <= d; ++i) {
-						if (i > 1
-							&& i < s.Length - 1
-							&& exists3blank == false) {
-							if (mass[i - 1] && mass[i] && mass[i + 1]) {
-								exists3blank = true;
-								break;
-							}
+			bool isOK = false;
+
+			int max = 100;
+			for (var p = 0; p <= max; ++p) { // a1
+				for (var q = 0; q <= max; ++q) { // a2
+					for (var k = 0; k <= max; ++k) { // a3
+						var l = c[0, 0] - p;
+						var m = c[0, 1] - p;
+						var n = c[0, 2] - p;
+						if (c[0, 0] == p + l
+							&& c[0, 1] == p + m
+							&& c[0, 2] == p + n
+							&& c[1, 0] == q + l
+							&& c[1, 1] == q + m
+							&& c[1, 2] == q + n
+							&& c[2, 0] == k + l
+							&& c[2, 1] == k + m
+							&& c[2, 2] == k + n) {
+							isOK = true;
+
+							break;
+						}
+
+						if (isOK) {
+							break;
 						}
 					}
 
-					isOK = exists3blank;
+					if (isOK) {
+						break;
+					}
+				}
+
+				if (isOK) {
+					break;
 				}
 			}
 
 			var answer = isOK ? "Yes" : "No";
 			Console.WriteLine($"{answer}");
-		}
-
-		public static void FriendsFriend()
-		{
-			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
-			var lists = new HashSet<int>[n];
-			for (var i = 0; i < n; ++i) {
-				lists[i] = new HashSet<int>();
-			}
-
-			for (var i = 0; i < m; ++i) {
-				var ab = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-				var a = ab[0] - 1;
-				var b = ab[1] - 1;
-				lists[a].Add(b);
-				lists[b].Add(a);
-			}
-
-			var builder = new StringBuilder();
-			for (var i = 0; i < n; ++i) {
-				var hash = new HashSet<int>();
-				foreach (var item in lists[i]) {
-					foreach (var item2 in lists[item]) {
-						if (hash.Contains(item2) == false && i != item2 && lists[i].Contains(item2) == false) {
-							hash.Add(item2);
-						}
-					}
-				}
-
-				builder.AppendLine($"{hash.Count}");
-			}
-
-			var answer = builder.ToString();
-			Console.Write($"{answer}");
 		}
 	}
 }
