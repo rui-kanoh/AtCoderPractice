dbb5cd7f22dfd22847110c471f6ad5b01ccad94a
elionix-kanoh <272-kanoh@elionix.com>
2023-02-22 12:35:40 +0900

    カンニングで三問目解いた

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -31,72 +31,71 @@ namespace AtCoderDotNetCore
 	{
 		public static void Exec()
 		{
-			ExecTemp();
+			ChangesUserNames();
 		}
 
-		public static void ExecTemp()
+		public class UnionFind
 		{
-			var nk = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var n = nk[0];
-			var k = nk[1];
+			List<int> parents;
 
-			var dict = new Dictionary<long, long>();
+			public int GroupCount { get; private set; }
 
-			var wlist = new List<long>();
-			var dlist = new long[n];
-
-			for (var i = 0; i < n; ++i) {
-				var wd = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-				var w = nk[0] - 1;
-				var d = nk[1];
-
-				wlist.Add(w);
-				dlist[i] = d;
+			public UnionFind(int x)
+			{
+				parents = Enumerable.Repeat(-1, x).ToList();
+				GroupCount = x;
 			}
 
-			wlist.Sort();
-
-			long count = 0;
-			while (count < k) {
-
-				if (k >= count) {
-					break;
+			public int Find(int x)
+			{
+				if (parents[x] < 0) {
+					return x;
+				} else {
+					parents[x] = Find(parents[x]);
+					return parents[x];
 				}
 			}
 
+			public void Union(int x, int y)
+			{
+				(x, y) = (Find(x), Find(y));
 
-			long x = -1;
-			foreach (var item in dict) {
-				count += item.Value;
-				if (count >= k) {
-					x = item.Key;
-					break;
+				if (x != y) {
+					if (Count(x) < Count(y)) (x, y) = (y, x);
+					parents[x] += parents[y];
+					parents[y] = x;
+					GroupCount--;
 				}
 			}
 
-			var answer = x;
-			Console.WriteLine($"{answer}");
+			public int Count(int x) => -parents[Find(x)];
+
+			public bool IsSame(int x, int y) => Find(x) == Find(y);
 		}
-	}
-}
 
-namespace AtCoderDotNetCore
-{
-	public class Template
-	{
 		public static void ChangesUserNames()
 		{
 			var n = int.Parse(Console.ReadLine());
 
-			var snames = new HashSet<string>();
+			var snames = new string[n];
 			var tnames = new string[n];
+			var dict = new Dictionary<string, int>();
 
-
-
+			int index = 0;
 			for (var i = 0; i < n; ++i) {
 				string[] st = Console.ReadLine().Split(" ");
-				snames.Add(st[0]);
+				snames[i] = st[0];
 				tnames[i] = st[1];
+
+				if (dict.ContainsKey(st[0]) == false) {
+					dict.Add(st[0], index);
+					++index;
+				}
+
+				if (dict.ContainsKey(st[1]) == false) {
+					dict.Add(st[1], index);
+					++index;
+				}
 			}
 
 			if (n == 1) {
@@ -104,19 +103,31 @@ namespace AtCoderDotNetCore
 				return;
 			}
 
-			bool isOK = false;
+			var union = new UnionFind(dict.Count);
 
+			bool isOK = true;
+
+			// 親が同じものがあれば閉路
 			for (var i = 0; i < n; ++i) {
-				if (snames.Contains(tnames[i]) == false) {
-					isOK = true;
+				if (union.IsSame(dict[snames[i]], dict[tnames[i]])) {
+					isOK = false;
 					break;
 				}
+
+				union.Union(dict[snames[i]], dict[tnames[i]]);
 			}
 
 			var answer = isOK ? "Yes" : "No";
 			Console.WriteLine($"{answer}");
 		}
 
+	}
+}
+
+namespace AtCoderDotNetCore
+{
+	public class Template
+	{
 		public static void ExecTemp()
 		{
 			string s = Console.ReadLine();
@@ -187,6 +198,51 @@ namespace AtCoderDotNetCore
 			var answer = builder.ToString();
 			Console.Write($"{answer}");
 		}
+
+		public static void D()
+		{
+			var nk = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var n = nk[0];
+			var k = nk[1];
+
+			var dict = new Dictionary<long, long>();
+
+			var wlist = new List<long>();
+			var dlist = new long[n];
+
+			for (var i = 0; i < n; ++i) {
+				var wd = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+				var w = nk[0] - 1;
+				var d = nk[1];
+
+				wlist.Add(w);
+				dlist[i] = d;
+			}
+
+			wlist.Sort();
+
+			long count = 0;
+			while (count < k) {
+
+				if (k >= count) {
+					break;
+				}
+			}
+
+
+			long x = -1;
+			foreach (var item in dict) {
+				count += item.Value;
+				if (count >= k) {
+					x = item.Key;
+					break;
+				}
+			}
+
+			var answer = x;
+			Console.WriteLine($"{answer}");
+		}
+
 	}
 }
 
