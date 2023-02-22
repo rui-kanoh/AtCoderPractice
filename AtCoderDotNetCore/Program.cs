62b7bef52d5bdd04b0e6e2ea6fedf15496de3521
elionix-kanoh <272-kanoh@elionix.com>
2023-02-22 09:20:09 +0900

    3問目WA

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -36,18 +36,26 @@ namespace AtCoderDotNetCore
 
 		public static void ExecTemp()
 		{
-			string s = Console.ReadLine();
+			var n = int.Parse(Console.ReadLine());
 
-			long ln = long.Parse(Console.ReadLine());
-			int n = int.Parse(Console.ReadLine());
+			var snames = new HashSet<string>();
+			var tnames = new string[n];
+			for (var i = 0; i < n; ++i) {
+				string[] st = Console.ReadLine().Split(" ");
+				snames.Add(st[0]);
+				tnames[i] = st[1];
+			}
 
-			string[] inputStrArray = Console.ReadLine().Split(" ");
+			bool isOK = false;
 
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			for (var i = 0; i < n; ++i) {
+				if (snames.Contains(tnames[i]) == false) {
+					isOK = true;
+					break;
+				}
+			}
 
-			var answer = 0;
-
+			var answer = isOK ? "Yes" : "No";
 			Console.WriteLine($"{answer}");
 		}
 	}
@@ -93,6 +101,39 @@ namespace AtCoderDotNetCore
 		{
 			long g = Gcd(a, b);
 			return a / g * b;
+		}
+
+		public static void A()
+		{
+			char X = char.Parse(Console.ReadLine());
+
+			var answer = X - 'A' + 1;
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void CountingRoads()
+		{
+			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+
+			var lists = new int[n];
+
+			for (var i = 0; i < m; ++i) {
+				var ab = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				var a = ab[0] - 1;
+				var b = ab[1] - 1;
+				++lists[a];
+				++lists[b];
+			}
+
+			var builder = new StringBuilder();
+			for (var i = 0; i < n; ++i) {
+				builder.AppendLine($"{lists[i]}");
+			}
+
+			var answer = builder.ToString();
+			Console.Write($"{answer}");
 		}
 	}
 }
