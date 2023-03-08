813c104f99c9268aedc959222fac5546aa3e1d7b
elionix-kanoh <272-kanoh@elionix.com>
2023-03-09 08:52:44 +0900

    貪欲寿司タワー

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -36,18 +36,30 @@ namespace AtCoderDotNetCore
 
 		public static void ExecTemp()
 		{
-			string s = Console.ReadLine();
-
-			long ln = long.Parse(Console.ReadLine());
 			int n = int.Parse(Console.ReadLine());
+			string s = Console.ReadLine();
+			var clist = s.ToCharArray().ToList();
 
-			string[] inputStrArray = Console.ReadLine().Split(" ");
+			// 後ろから見て行って01が連続したらRemove
+			// 残ったものの個数/2が答え
+			bool canRemove = true;
+			for (var i = 1; i < 2 * n; ++i) {
+				int index = 2 * n - 1 - i;
+				if (clist[index] != clist[index + 1]) {
+					if (canRemove) {
+						clist.RemoveAt(index + 1);
+						clist.RemoveAt(index);
+						canRemove = false;
+						continue;
+					}
+				}
 
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+				if (canRemove == false) {
+					canRemove = true;
+				}
+			}
 
-			var answer = 0;
-
+			var answer = clist.Count / 2;
 			Console.WriteLine($"{answer}");
 		}
 	}
@@ -93,6 +105,87 @@ namespace AtCoderDotNetCore
 		{
 			long g = Gcd(a, b);
 			return a / g * b;
+		}
+
+		public static void HD()
+		{
+			string[] ab = Console.ReadLine().Split(" ");
+
+			var answer = (ab[0] == "H" && ab[1] == "H") || (ab[0] == "D" && ab[1] == "D")
+				? "H"
+				: "D";
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void CollectingBalls()
+		{
+			int n = int.Parse(Console.ReadLine());
+			int k = int.Parse(Console.ReadLine());
+			var x = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+
+			long total = 0;
+			for (var i = 0; i < n; ++i) {
+				total += Math.Min(x[i], Math.Abs(k - x[i])) * 2;
+			}
+
+			var answer = total;
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void Dangeras()
+		{
+			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nm[0];
+			var m = nm[1];
+			var list = new HashSet<int>[m];
+			for (var i = 0; i < m; ++i) {
+				var abc = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				list[i] = new HashSet<int> {
+					abc[0] - 1,
+					abc[1] - 1,
+					abc[2] - 1,
+				};
+			}
+
+			if (n == 3 || m == 1) {
+				Console.WriteLine("1");
+				return;
+			}
+
+			long max = 0;
+			for (var i = 0; i < n; ++i) {
+				for (var j = i + 1; j < n; ++j) {
+					if (i == j) {
+						continue;
+					}
+
+					var hash = new HashSet<int>();
+					foreach (var item in list) {
+						if (item.Contains(i) && item.Contains(j)) {
+							int k = -1;
+							if (item.ElementAt(0) == i && item.ElementAt(1) == j) {
+								k = item.ElementAt(2);
+							} else {
+								if (item.ElementAt(0) == i && item.ElementAt(2) == j) {
+									k = item.ElementAt(1);
+								} else {
+									k = item.ElementAt(0);
+								}
+							}
+
+							if (hash.Contains(k) == false) {
+								hash.Add(k);
+							}
+						}
+					}
+
+					max = Math.Max(max, hash.Count);
+				}
+			}
+
+			var answer = max;
+
+			Console.WriteLine($"{answer}");
 		}
 	}
 }
