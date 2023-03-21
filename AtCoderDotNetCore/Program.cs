a6d4c7ab77233d43fab1e77a7d9d93e20e46ba08
elionix-kanoh <272-kanoh@elionix.com>
2023-03-21 12:38:56 +0900

    3問目WAまで行けた

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -16,24 +16,6 @@ using System.Runtime.Intrinsics.X86;
 using System.Reflection;
 using System.Drawing;
 using System.Net;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.Linq;
-using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
-using System.Buffers.Text;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.Runtime.Intrinsics.X86;
-using System.Reflection;
-using System.Drawing;
-using System.Net;
 
 namespace AtCoderDotNetCore
 {
@@ -52,74 +34,39 @@ namespace AtCoderDotNetCore
 			ExecTemp();
 		}
 
+		public static bool Check(int value, int[] list)
+		{
+			var num = list.Distinct().Count(x => x <= value);
+			var amari = list.Length - num;
+			bool isOK = value <= num + (amari / 2); 
+
+			return isOK;
+		}
+
+		public static (int ok, int ng) BinarySearch(int[] list)
+		{
+			int ok = 0;
+			int ng = list.Length;
+			while (ng - ok > 1) {
+				int mid = (ng + ok) / 2;
+				if (Check(mid, list)) {
+					ok = mid;
+				} else {
+					ng = mid;
+				}
+			}
+
+			return (ok, ng);
+		}
+
 		public static void ExecTemp()
 		{
-			var ij = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
-			var indexI = ij[0];
-			var indexJ = ij[1];
+			long n = long.Parse(Console.ReadLine());
+			var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
 
-			long Calc(long k)
-			{
-				return k * (k + 1) / 2;
-			}
+			(int ok, int ng) = BinarySearch(a);
 
-			// i番目の組
-			long group1 = 0;
-			long rem1 = 0;
-			bool found1 = false;
-
-			long group2 = 0;
-			long rem2 = 0;
-			bool found2 = false;
-
-			for (long k = 1; k < long.MaxValue; ++k) {
-				if (indexI <= Calc(k) && found1 == false) {
-					group1 = k;
-					rem1 = indexI == Calc(k)
-						? 0
-						: indexI - Calc(k - 1);
-					found1 = true;
-				}
-
-				if (indexJ <= Calc(k) && found2 == false) {
-					group2 = k;
-					rem2 = indexJ == Calc(k)
-						? 0
-						: indexJ - Calc(k - 1);
-					found2 = true;
-				}
-
-				if (found1 && found2) {
-					break;
-				}
-			}
-
-			long numIa = 0;
-			long numIb = 0;
-			if (rem1 == 0) {
-				numIb = group1;
-				numIa = 1;
-			} else {
-				numIb = rem1;
-				numIa = group1 - numIb + 1;
-			}
-
-			long numJa = 0;
-			long numJb = 0;
-			if (rem2 == 0) {
-				numJb = group2;
-				numJa = 1;
-			} else {
-				numJb = rem2;
-				numJa = group2 - numJb + 1;
-			}
-
-			var numA = numIa + numJa;
-			var numB = numIb + numJb;
-
-			var newGroupNum = numA + numB - 1;
-
-			var answer = Calc(newGroupNum - 1) + newGroupNum - numA + 1;
+			var answer = ok;
 			Console.WriteLine($"{answer}");
 		}
 	}
@@ -167,177 +114,25 @@ namespace AtCoderDotNetCore
 			return a / g * b;
 		}
 
-		public static void NewYear()
+		public static void Exec229()
 		{
-			int m = int.Parse(Console.ReadLine());
-			var answer = 24 + 24 - m;
-			Console.WriteLine($"{answer}");
-		}
+			var ab = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+			var a = ab[0];
+			var b = ab[1];
+			long count = (b / 4) - (b / 100) + (b / 400);
+			count -= ((a - 1) / 4) - ((a - 1) / 100) + ((a - 1) / 400);
 
-		public static void CenterSaiten()
-		{
-			int n = int.Parse(Console.ReadLine());
-			string s = Console.ReadLine();
-			var clist = s.ToCharArray();
-			var counts = new[] { clist.Count(s => s == '1'), clist.Count(s => s == '2'), clist.Count(s => s == '3'), clist.Count(s => s == '4'), };
-			Console.WriteLine($"{counts.Max()} {counts.Min()}");
-		}
-
-		public static void LeadingZeros()
-		{
-			int n = int.Parse(Console.ReadLine());
-
-			var dict = new SortedDictionary<BigInteger, List<(string str, int count)>>();
-
-			for (var i = 0; i < n; ++i) {
-				string s = Console.ReadLine();
-				var num = BigInteger.Parse(s);
-				if (dict.ContainsKey(num) == false) {
-					dict.Add(num, new List<(string str, int count)>());
-				}
-
-				int countZero = 0;
-				for (var j = 0; j < s.Length; ++j) {
-					if (s[j] == '0') {
-						++countZero;
-					} else {
-						break;
-					}
-				}
-
-				dict[num].Add((s, countZero));
-			}
-
-			foreach (var item in dict) {
-				item.Value.Sort((a, b) => b.count.CompareTo(a.count));
-			}
-
-			var builder = new StringBuilder();
-			foreach (var item in dict) {
-				foreach (var c in item.Value) {
-					builder.AppendLine(c.str);
+			// 愚直になげるとTLE
+			/*
+			for (long i = a; i <= b; ++i) {
+				if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+					++count;
 				}
 			}
+			*/
 
-			var answer = builder.ToString();
+			var answer = count;
 			Console.Write($"{answer}");
-		}
-
-		public static void HD()
-		{
-			string[] ab = Console.ReadLine().Split(" ");
-
-			var answer = (ab[0] == "H" && ab[1] == "H") || (ab[0] == "D" && ab[1] == "D")
-				? "H"
-				: "D";
-			Console.WriteLine($"{answer}");
-		}
-
-		public static void CollectingBalls()
-		{
-			int n = int.Parse(Console.ReadLine());
-			int k = int.Parse(Console.ReadLine());
-			var x = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-
-			long total = 0;
-			for (var i = 0; i < n; ++i) {
-				total += Math.Min(x[i], Math.Abs(k - x[i])) * 2;
-			}
-
-			var answer = total;
-			Console.WriteLine($"{answer}");
-		}
-
-		public static void Dangeras()
-		{
-			var nm = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var n = nm[0];
-			var m = nm[1];
-			var list = new HashSet<int>[m];
-			for (var i = 0; i < m; ++i) {
-				var abc = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-				list[i] = new HashSet<int> {
-					abc[0] - 1,
-					abc[1] - 1,
-					abc[2] - 1,
-				};
-			}
-
-			if (n == 3 || m == 1) {
-				Console.WriteLine("1");
-				return;
-			}
-
-			long max = 0;
-			for (var i = 0; i < n; ++i) {
-				for (var j = i + 1; j < n; ++j) {
-					if (i == j) {
-						continue;
-					}
-
-					var hash = new HashSet<int>();
-					foreach (var item in list) {
-						if (item.Contains(i) && item.Contains(j)) {
-							int k = -1;
-							if (item.ElementAt(0) == i && item.ElementAt(1) == j) {
-								k = item.ElementAt(2);
-							} else {
-								if (item.ElementAt(0) == i && item.ElementAt(2) == j) {
-									k = item.ElementAt(1);
-								} else {
-									k = item.ElementAt(0);
-								}
-							}
-
-							if (hash.Contains(k) == false) {
-								hash.Add(k);
-							}
-						}
-					}
-
-					max = Math.Max(max, hash.Count);
-				}
-			}
-
-			var answer = max;
-
-			Console.WriteLine($"{answer}");
-		}
-
-		public static void 寿司タワー貪欲法()
-		{
-			int n = int.Parse(Console.ReadLine());
-			string s = Console.ReadLine();
-			var clist = s.ToCharArray().ToList();
-
-			if (n == 1) {
-				Console.WriteLine($"0");
-				return;
-			}
-
-			// 後ろから見て行って01が連続したらRemove
-			// 残ったものの個数/2が答え
-			bool canRemove = true;
-			for (var i = 1; i < 2 * n; ++i) {
-				int index = 2 * n - 1 - i;
-				if (clist.Count > index + 1) {
-					if (clist[index] != clist[index + 1]) {
-						if (canRemove) {
-							clist.RemoveAt(index + 1);
-							clist.RemoveAt(index);
-							canRemove = false;
-							continue;
-						}
-					}
-				}
-
-				if (canRemove == false) {
-					canRemove = true;
-				}
-			}
-
-			var answer = clist.Count / 2;
-			Console.WriteLine($"{answer}");
 		}
 	}
 }
