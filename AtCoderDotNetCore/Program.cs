4f9498bfdabaad93356139aee60c86f7bf538acd
elionix-kanoh <272-kanoh@elionix.com>
2023-08-30 10:24:47 +0900

    3問目までは解けた

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -31,6 +31,44 @@ namespace AtCoderDotNetCore
 		}
 	}
 
+	public class UnionFind
+	{
+		List<int> parents;
+
+		public int GroupCount { get; private set; }
+
+		public UnionFind(int x)
+		{
+			parents = Enumerable.Repeat(-1, x).ToList();
+			GroupCount = x;
+		}
+
+		public int Find(int x)
+		{
+			if (parents[x] < 0) return x;
+			else {
+				parents[x] = Find(parents[x]);
+				return parents[x];
+			}
+		}
+
+		public void Union(int x, int y)
+		{
+			(x, y) = (Find(x), Find(y));
+
+			if (x != y) {
+				if (Count(x) < Count(y)) (x, y) = (y, x);
+				parents[x] += parents[y];
+				parents[y] = x;
+				GroupCount--;
+			}
+		}
+
+		public int Count(int x) => -parents[Find(x)];
+
+		public bool IsSame(int x, int y) => Find(x) == Find(y);
+	}
+
 	public static class Question
 	{
 		public static void Exec()
@@ -40,19 +78,38 @@ namespace AtCoderDotNetCore
 
 		public static void ExecTemp()
 		{
-			string s = Console.ReadLine();
+			var nk = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var n = nk[0];
+			var k = nk[1];
 
-			long ln = long.Parse(Console.ReadLine());
-			int n = int.Parse(Console.ReadLine());
+			var dict = new Dictionary<int, int>();
 
-			string[] inputStrArray = Console.ReadLine().Split(" ");
+			var cards = new List<(int number, int index)>();
+			bool hasBlank = false;
+			for (var i = 0; i < n; ++i) {
+				int card = int.Parse(Console.ReadLine());
+				if (card > 0) {
+					cards.Add((card, i));
+				} else {
+					hasBlank = true;
+				}
 
-			var array = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var larray = Console.ReadLine().Split(" ").Select(i => long.Parse(i)).ToArray();
+				dict.Add(i, card);
+			}
 
-			var answer = 0;
+			if (n <= 1000 && k <= 500) {
+				// 全探索
+				cards.Sort((a, b) => a.number.CompareTo(b.number));
 
-			Console.WriteLine($"{answer}");
+				for (var i = 1; i < cards.Count; ++i) {
+					if (cards[i].number - cards[i - 1].number == 1) {
+
+					}
+				}
+
+				var answer = 0;
+				Console.WriteLine($"{answer}");
+			}
 		}
 	}
 }
@@ -99,42 +156,106 @@ namespace AtCoderDotNetCore
 			return a / g * b;
 		}
 
-		public static void TwoDigit()
+		public static void Theater()
 		{
-			int a = int.Parse(Console.ReadLine());
-			int b = int.Parse(Console.ReadLine());
+			int n = int.Parse(Console.ReadLine());
 
-			var answer = a * 10 + b;
+			long total = 0;
+			for (var i = 0; i < n; ++i) {
+				var lr = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				var l = lr[0];
+				var r = lr[1];
+				total += (r - l + 1);
+			}
+
+			var answer = total;
 			Console.WriteLine($"{answer}");
 		}
 
-		public static void Zenkan()
+		public static void SakasamaJisyo()
 		{
-			var mb = new string[10];
-			for (var i = 0; i < 10; ++i) {
-				mb[i] = Console.ReadLine();
+			int n = int.Parse(Console.ReadLine());
+
+			var dict = new Dictionary<string, string>();
+			var list = new List<string>();
+			for (var i = 0; i < n; ++i) {
+				string s = Console.ReadLine();
+				string rev = new string(s.Reverse().ToArray());
+				list.Add(rev);
+				dict.Add(rev, s);
 			}
 
-			bool isOK = true;
+			list.Sort();
 
-			for (var i = 0; i < 10; ++i) {
-				bool isOKLocal = false;
-				for (var j = 0; j < 10; ++j) {
-					if (mb[j][i] == 'o') {
-						isOKLocal = true;
-						break;
+			var builder = new StringBuilder();
+			for (var i = 0; i < n; ++i) {
+				builder.AppendLine(dict[list[i]]);
+			}
+
+			var answer = builder.ToString();
+			Console.Write($"{answer}");
+		}
+
+		public static void Collision2()
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			var dict = new Dictionary<int, List<(int pos, bool isRight)>>();
+
+			var xylist = new List<(int x, int y)>();
+			for (var i = 0; i < n; ++i) {
+				var xy = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+				var x = xy[0];
+				var y = xy[1];
+				xylist.Add((x, y));
+			}
+
+			bool hasColl = false;
+
+			string s = Console.ReadLine();
+
+			for (var i = 0; i < n; ++i) {
+				if (dict.ContainsKey(xylist[i].y) == false) {
+					dict.Add(xylist[i].y, new List<(int, bool)>());
+				}
+
+				dict[xylist[i].y].Add((xylist[i].x, s[i] == 'R'));
+			}
+
+			// 各Yについて衝突が発生するかどうかチェックする
+			foreach (var key in dict.Keys) {
+				bool hasColl2 = false;
+				var list = dict[key];
+
+				// ソートして、Rに進むやつが出てくるまで見て、それいこうでLが出たら衝突
+				list.Sort((a, b) => a.pos.CompareTo(b.pos));
+
+				bool right = false;
+				foreach ((var pos, bool isRight) in list) {
+					if (right == false) {
+						if (isRight == false) {
+							continue;
+						} else {
+							right = true;
+						}
+					} else {
+						if (isRight == false) {
+							hasColl2 = true;
+							break;
+						}
 					}
 				}
 
-				if (isOKLocal == false) {
-					isOK = false;
+				if (hasColl2) {
+					hasColl = true;
 					break;
 				}
 			}
 
-			var answer = isOK ? "Yes" : "No";
+			var answer = hasColl ? "Yes" : "No";
 			Console.WriteLine($"{answer}");
 		}
 	}
+}
 }
 
