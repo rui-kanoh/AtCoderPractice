39019cb95e711932c73752a91ed8d7d918b3e0d9
elionix-kanoh <272-kanoh@elionix.com>
2023-09-13 10:06:49 +0900

    3問目ダメだった

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -76,144 +76,146 @@ namespace AtCoderDotNetCore
 			ExecTemp();
 		}
 
-		public static (int step, bool canReach, int fx, int fy) GridBFS(int h, int w, int sx, int sy, int gx, int gy, bool[,] map)
-		{
-			int[] vx = { 0, 1, 0, -1 };
-			int[] vy = { 1, 0, -1, 0 };
-
-			if (map[sx, sy] == false || map[gx, gy] == false) {
-				return (0, false, 0, 0);
-			}
-
-			if (sx == gx && sy == gy) {
-				return (0, false, 0, 0);
-			}
-
-			var dist = new bool[h, w];
-			var tq = new Queue<(int, int, int)>();
-			int step = 0;
-			tq.Enqueue((sx, sy, step));
-			dist[sx, sy] = true;
-
-			bool canReach = false;
-
-			int fx = 0;
-			int fy = 0;
-			while (0 < tq.Count) {
-				var q = tq.Dequeue();
-				int x = q.Item1;
-				int y = q.Item2;
-				step = q.Item3;
-
-				if (x == gx && y == gy) {
-					canReach = true;
-					break;
-				}
-
-				for (int i = 0; i < 4; i++) {
-					int nx = x + vx[i];
-					int ny = y + vy[i];
-					if ((0 <= nx && nx < h) && (0 <= ny && ny < w) && dist[nx, ny] == false
-						&& map[nx, ny]) {
-						/*
-						if (map[nx, ny] == false) {
-							if (brokenCount < 2) {
-								++brokenCount;
-								map[nx, ny] = true;
-							} else {
-								continue;
-							}
-						}
-						*/
-
-						dist[nx, ny] = true;
-						tq.Enqueue((nx, ny, step + 1));
-					}
-				}
-
-				if (0 == tq.Count) {
-					Console.WriteLine($"{x} {y}");
-					fx = x;
-					fy = y;
-					break;
-				}
-			}
-
-			return (step, canReach, fx, fy);
-		}
-
 		public static void ExecTemp()
 		{
-			var hw = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var h = hw[0];
-			var w = hw[1];
+			long n = long.Parse(Console.ReadLine());
 
-			var map = new bool[h, w];
+			// n - (a^b で表せるもの）が答え
+			// b = 2 で固定すると aはSQRT(N)まででよい。
 
-			int sx = 0;
-			int sy = 0;
-			int gx = 0;
-			int gy = 0;
-			for (var i = 0; i < h; ++i) {
-				var temp = Console.ReadLine();
-				for (var j = 0; j < w; ++j) {
-					if (temp[j] is 's' or 'g' or '.') {
-						map[i, j] = true;
+			// 階乗の個数の計算
+			int bcount = 0;
+			for (var a = 2; a <= 10; ++a) {
+				if (Math.Pow((double)n, 1.0 / (double)a) < 2) {
+					bcount = a - 1;
+					break;
+				}
 
-						if (temp[j] is 's') {
-							sx = i;
-							sy = j;
-						} else if (temp[j] is 'g') {
-							gx = i;
-							gy = j;
-						}
-					} else {
-						map[i, j] = false;
-					}
+				if (a == 10) {
+					bcount = 10;
 				}
 			}
 
-			(int step, bool canReach, int fx, int fy) = GridBFS(h, w, sx, sy, gx, gy, map);
+			long count = 0;
 
-			if (fx == gx && fy == gy) {
-				canReach = true;
-			} else {
-				int[] vx = { 0, 1, 0, -1 };
-				int[] vy = { 1, 0, -1, 0 };
-				for (int i = 0; i < 4; i++) {
-					int nx = fx + vx[i];
-					int ny = fy + vy[i];
-					if ((0 <= nx && nx < h) && (0 <= ny && ny < w)) {
-						map[nx, ny] = true;
-						if (map[nx, ny]) {
-							(step, canReach, fx, fy) = GridBFS(h, w, sx, sy, gx, gy, map);
-							if (canReach) {
-								break;
+			var hash = new HashSet<long>();
+
+			// b = 2
+			for (var a = 2; a * a <= n; ++a) {
+				long value = a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			// b = 3
+			for (var a = 2; a * a * a <= n; ++a) {
+				long value = a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			if (bcount > 3) {
+				/*
+				// b = 4
+				for (var a = 2; a * a * a * a <= n; ++a) {
+					long value = a * a * a * a;
+
+					if (hash.Contains(value) == false) {
+						++count;
+
+						hash.Add(value);
+					}
+				}
+				*/
+
+				if (bcount > 4) {
+					// b = 5
+					for (var a = 2; a * a * a * a * a <= n; ++a) {
+						long value = a * a * a * a * a;
+
+						if (hash.Contains(value) == false) {
+							++count;
+
+							if (value == 100000) {
+								Console.WriteLine($"{value}");
+							}
+
+							hash.Add(value);
+						}
+					}
+
+					/*
+					// b = 6
+					for (var a = 2; a * a * a * a * a * a <= n; ++a) {
+						long value = a * a * a * a * a * a;
+
+						if (hash.Contains(value) == false) {
+							++count;
+
+							hash.Add(value);
+						}
+					}
+					*/
+
+					if (bcount > 6) {
+						// b = 7
+						for (var a = 2; a * a * a * a * a * a * a <= n; ++a) {
+							long value = a * a * a * a * a * a * a;
+
+							if (hash.Contains(value) == false) {
+								++count;
+
+								hash.Add(value);
 							}
 						}
 					}
 				}
 
-				if (fx == gx && fy == gy) {
-					canReach = true;
-				} else {
-					for (int i = 0; i < 4; i++) {
-						int nx = fx + vx[i];
-						int ny = fy + vy[i];
-						if ((0 <= nx && nx < h) && (0 <= ny && ny < w)) {
-							map[nx, ny] = true;
-							if (map[nx, ny]) {
-								(step, canReach, fx, fy) = GridBFS(h, w, sx, sy, gx, gy, map);
-								if (canReach) {
-									break;
-								}
-							}
-						}
+				/*
+				// b = 8
+				for (var a = 2; a * a * a * a * a * a * a * a <= n; ++a) {
+					long value = a * a * a * a * a * a * a * a;
+
+					if (hash.Contains(value) == false) {
+						++count;
+
+						hash.Add(value);
 					}
 				}
+
+				// b = 9
+				for (var a = 2; a * a * a * a * a * a * a * a * a<= n; ++a) {
+					long value = a * a * a * a * a * a * a * a * a;
+
+					if (hash.Contains(value) == false) {
+						++count;
+
+						hash.Add(value);
+					}
+				}
+
+				// b = 10
+				for (var a = 2; a * a * a * a * a * a * a * a * a * a <= n; ++a) {
+					long value = a * a * a * a * a * a * a * a * a * a;
+
+					if (hash.Contains(value) == false) {
+						++count;
+
+						hash.Add(value);
+					}
+				}
+				*/
 			}
 
-			var answer = canReach ? "YES" : "NO";
+			var answer = n - count;
 			Console.WriteLine($"{answer}");
 		}
 	}
@@ -261,33 +263,154 @@ namespace AtCoderDotNetCore
 			return a / g * b;
 		}
 
-		public static void Tako()
+		public static void TaskScheduling()
 		{
-			var xy = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
-			var x = xy[0];
-			var y = xy[1];
+			var a = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToList();
+			a.Sort();
 
-			var answer = y / x;
+			var answer = Math.Abs(a[1] - a[0]) + Math.Abs(a[2] - a[1]);
 			Console.WriteLine($"{answer}");
 		}
 
-		public static void Overwrite()
+		public static void SportsDay()
 		{
 			int n = int.Parse(Console.ReadLine());
+			int k = int.Parse(Console.ReadLine());
 			string s = Console.ReadLine();
 
-			var list = s.ToCharArray().ToList();
-			var tlist = new List<char>();
+			bool isRed = true;
 
-			for (var i = 0; i < list.Count; ++i) {
-				if (tlist.Contains(s[i])) {
-					tlist.RemoveAll(c => c == s[i]);
+			int count = 0;
+			for (var i = 0; i < n - 1; ++i) {
+				if (s[i] == 'R') {
+					++count;
+
+					if (count == k) {
+						isRed = false;
+						break;
+					}
 				}
-
-				tlist.Add(s[i]);
 			}
 
-			var answer = new string(tlist.ToArray());
+			var answer = isRed ? "R" : "W";
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void Unexpressed()
+		{
+			long n = long.Parse(Console.ReadLine());
+
+			// n - (a^b で表せるもの）が答え
+			// b = 2 で固定すると aはSQRT(N)まででよい。
+
+			long count = 0;
+
+			var hash = new HashSet<long>();
+
+			// b = 2
+			for (var a = 2; a * a <= n; ++a) {
+				long value = a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			// b = 3
+			for (var a = 2; a * a * a <= n; ++a) {
+				long value = a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			/*
+			// b = 4
+			for (var a = 2; a * a * a * a <= n; ++a) {
+				long value = a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+			*/
+
+			// b = 5
+			for (var a = 2; a * a * a * a * a <= n; ++a) {
+				long value = a * a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			// b = 6
+			for (var a = 2; a * a * a * a * a * a <= n; ++a) {
+				long value = a * a * a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			// b = 7
+			for (var a = 2; a * a * a * a * a * a * a <= n; ++a) {
+				long value = a * a * a * a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			/*
+			// b = 8
+			for (var a = 2; a * a * a * a * a * a * a * a <= n; ++a) {
+				long value = a * a * a * a * a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			// b = 9
+			for (var a = 2; a * a * a * a * a * a * a * a * a<= n; ++a) {
+				long value = a * a * a * a * a * a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+			*/
+
+			// b = 10
+			for (var a = 2; a * a * a * a * a * a * a * a * a * a <= n; ++a) {
+				long value = a * a * a * a * a * a * a * a * a * a;
+
+				if (hash.Contains(value) == false) {
+					++count;
+
+					hash.Add(value);
+				}
+			}
+
+			var answer = n - count;
 			Console.WriteLine($"{answer}");
 		}
 	}
