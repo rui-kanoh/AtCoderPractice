0899dbceb45a4f9b9f5a7f93ff44089da1285b15
elionix-kanoh <272-kanoh@elionix.com>
2023-09-20 11:42:02 +0900

    3問目も４問目も溶けなかった

--- AtCoderDotNetCore/Program.cs
+++ AtCoderDotNetCore/Program.cs
@@ -40,28 +40,113 @@ namespace AtCoderDotNetCore
 
 		public static void ExecTemp()
 		{
-			long n = long.Parse(Console.ReadLine());
+			var hw = Console.ReadLine().Split(" ").Select(i => int.Parse(i)).ToArray();
+			var h = hw[0];
+			var w = hw[1];
 
-			var hash = new HashSet<long>();
+			var pazzel = new char[h, w];
 
-			for (long a = 2; a * a <= n; ++a) {
-				for (long b = 2; b <= (int)Math.Log2(n); ++b) {
-					long value = a;
-					for (long i = 2; i <= b; ++i) {
-						value *= a;
-					}
+			var array = new (bool isBlock, int label)[h, w];
 
-					if (value > n) {
-						break;
-					}
+			for (var i = 0; i < h; ++i) {
+				string s = Console.ReadLine();
+				for (var j = 0; j < w; ++j) {
+					pazzel[i, j] = s[j];
+				}
+			}
 
-					if (hash.Contains(value) == false) {
-						hash.Add(value);
+
+			int label = 1;
+			for (var i = 0; i < h; ++i) {
+				for (var j = 2; j < w; ++j) {
+					if (pazzel[i, j - 2] == pazzel[i, j - 1] && pazzel[i, j - 2] == pazzel[i, j]) {
+
+						if (array[i, j - 2].isBlock == false && array[i, j - 1].isBlock == false) {
+							if (i >= 1
+								&& ((array[i - 1, j - 2].isBlock && pazzel[i - 1, j - 2] == pazzel[i, j])
+								|| (array[i - 1, j - 1].isBlock && pazzel[i - 1, j - 1] == pazzel[i, j])
+								|| (array[i - 1, j - 0].isBlock && pazzel[i - 1, j - 0] == pazzel[i, j]))) {
+								if (array[i - 1, j - 2].isBlock) {
+									array[i, j - 2] = (true, array[i - 1, j - 2].label);
+									array[i, j - 1] = (true, array[i - 1, j - 2].label);
+									array[i, j - 0] = (true, array[i - 1, j - 2].label);
+								}
+
+								if (array[i - 1, j - 1].isBlock) {
+									array[i, j - 2] = (true, array[i - 1, j - 1].label);
+									array[i, j - 1] = (true, array[i - 1, j - 1].label);
+									array[i, j - 0] = (true, array[i - 1, j - 1].label);
+								}
+
+								if (array[i - 1, j - 0].isBlock) {
+									array[i, j - 2] = (true, array[i - 1, j - 0].label);
+									array[i, j - 1] = (true, array[i - 1, j - 0].label);
+									array[i, j - 0] = (true, array[i - 1, j - 0].label);
+								}
+							} else {
+								array[i, j - 2] = (true, label);
+								array[i, j - 1] = (true, label);
+								array[i, j - 0] = (true, label);
+
+								++label;
+							}
+						} else {
+							array[i, j - 2] = (true, array[i - 1, j - 0].label);
+							array[i, j - 1] = (true, array[i - 1, j - 0].label);
+							array[i, j - 0] = (true, array[i - 1, j - 0].label);
+						}
 					}
 				}
 			}
 
-			var answer = n - hash.Count;
+			for (var i = 0; i < h; ++i) {
+				for (var j = 0; j < w; ++j) {
+					Console.Write($"{array[i, j].label} ");
+				}
+
+				Console.WriteLine($"");
+			}
+
+			Console.WriteLine($"");
+
+			for (var j = 0; j < w; ++j) {
+				for (var i = 2; i < h; ++i) {
+					if (pazzel[i - 2, j] == pazzel[i - 1, j] && pazzel[i - 2, j] == pazzel[i, j]) {
+						if (array[i - 2, j].isBlock == false && array[i - 1, j].isBlock == false && array[i, j].isBlock == false) {
+							array[i - 2, j] = (true, label);
+							array[i - 1, j] = (true, label);
+							array[i - 0, j] = (true, label);
+
+							++label;
+						} else {
+							if (array[i - 2, j].isBlock) {
+								array[i - 1, j] = (true, array[i - 2, j].label);
+								array[i - 0, j] = (true, array[i - 2, j].label);
+							}
+
+							if (array[i - 1, j].isBlock) {
+								array[i - 2, j] = (true, array[i - 1, j].label);
+								array[i - 0, j] = (true, array[i - 1, j].label);
+							}
+
+							if (array[i, j].isBlock) {
+								array[i - 2, j] = (true, array[i - 0, j].label);
+								array[i - 1, j] = (true, array[i - 0, j].label);
+							}
+						}
+					}
+				}
+			}
+
+			for (var i = 0; i < h; ++i) {
+				for (var j = 0; j < w; ++j) {
+					Console.Write($"{array[i, j].label} ");
+				}
+
+				Console.WriteLine($"");
+			}
+
+			var answer = label - 1;
 			Console.WriteLine($"{answer}");
 		}
 	}
@@ -107,6 +192,41 @@ namespace AtCoderDotNetCore
 		{
 			long g = Gcd(a, b);
 			return a / g * b;
+		}
+
+		public static void i18n()
+		{
+			string s = Console.ReadLine();
+			var answer = $"{s[0]}{s.Length - 2}{s[s.Length - 1]}";
+			Console.WriteLine($"{answer}");
+		}
+
+		public static void Fox()
+		{
+			int n = int.Parse(Console.ReadLine());
+			string s = Console.ReadLine();
+
+			var builder = new StringBuilder();
+			for (var i = 0; i < s.Length; ++i) {
+				if (s[i] is not 'f' and not 'o' and not 'x') {
+					builder.Append('?');
+				} else {
+					builder.Append(s[i]);
+				}
+			}
+
+			s = builder.ToString();
+
+			var hash = s.ToCharArray().ToHashSet();
+
+			while (hash.Contains('f') && hash.Contains('o') && hash.Contains('x')) {
+				s = s.Replace("fox", "");
+
+				hash = s.ToCharArray().ToHashSet();
+			}
+
+			var answer = s.Length;
+			Console.WriteLine($"{answer}");
 		}
 
 		public static void TaskScheduling()
